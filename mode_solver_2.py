58f62a9a323397d4bfe6e7b9182e1373056f2bf0
HelgeGehring <HelgeGehring@gmail.com>
2022-10-27 10:34:21 -0700

    cleanup

--- mode_solver_2.py
+++ mode_solver_2.py
@@ -1,11 +1,9 @@
 """Waveguide analysis based on https://doi.org/10.1080/02726340290084012."""
 import numpy as np
-import scipy.linalg
-import scipy.sparse.linalg
 
 import skfem
-from skfem import *
-from skfem.helpers import *
+from skfem import BilinearForm, Basis, ElementTriN0, ElementTriP0, ElementTriP1, ElementVector
+from skfem.helpers import curl, grad, dot, inner
 
 mesh = skfem.Mesh.load('mesh.msh')
 basis = Basis(mesh, ElementTriN0() * ElementTriP1())
@@ -23,9 +21,10 @@ one_over_u_r = 1
 
 @BilinearForm
 def aform(E_t, E_z, v_t, v_z, w):
-    return one_over_u_r * curl(E_t) * curl(v_t) - k0 ** 2 * w['epsilon'] * dot(E_t, v_t) - one_over_u_r * dot(grad(E_z),
-                                                                                                              v_t) + w[
-               'epsilon'] * inner(E_t, grad(v_z)) + w['epsilon'] * E_z * v_z
+    return one_over_u_r * curl(E_t) * curl(v_t) \
+           - k0 ** 2 * w['epsilon'] * dot(E_t, v_t) \
+           - one_over_u_r * dot(grad(E_z), v_t) \
+           + w['epsilon'] * inner(E_t, grad(v_z)) + w['epsilon'] * E_z * v_z
 
 
 @BilinearForm
@@ -37,17 +36,12 @@ A = aform.assemble(basis, epsilon=basis0.interpolate(epsilon))
 B = bform.assemble(basis, epsilon=basis0.interpolate(epsilon))
 
 # lams, xs = solve(*condense(A, B, D=basis.get_dofs()),
-#                 solver=solver_eigen_scipy_sym(k=10, sigma=k0 ** 2 * 2.5 ** 2))
+#                solver=solver_eigen_scipy_sym(k=10, sigma=k0 ** 2 * 2.5 ** 2))
 from petsc4py import PETSc
 from slepc4py import SLEPc
 
-A_ = PETSc.Mat().createAIJ(size=A.shape,
-                           csr=(A.indptr, A.indices,
-                                A.data))
-
-B_ = PETSc.Mat().createAIJ(size=B.shape,
-                           csr=(B.indptr, B.indices,
-                                B.data))
+A_ = PETSc.Mat().createAIJ(size=A.shape, csr=(A.indptr, A.indices, A.data))
+B_ = PETSc.Mat().createAIJ(size=B.shape, csr=(B.indptr, B.indices, B.data))
 
 eps = SLEPc.EPS().create()
 eps.setOperators(A_, B_)
@@ -55,7 +49,7 @@ eps.setType(SLEPc.EPS.Type.KRYLOVSCHUR)
 st = eps.getST()
 st.setType(SLEPc.ST.Type.SINVERT)
 eps.setWhichEigenpairs(SLEPc.EPS.Which.TARGET_MAGNITUDE)
-eps.setTarget(100)
+eps.setTarget(k0 ** 2 * np.max(epsilon) ** 2)
 eps.setDimensions(20)
 eps.solve()
 
@@ -68,19 +62,12 @@ for i in range(eps.getConverged()):
 lams = np.array(lams)
 xs = np.array(xs).T
 
-# lams, xs = scipy.linalg.eig(A.todense(), B.todense())
-# lams, xs = scipy.sparse.linalg.eigs(A, M=B, which='LR', sigma=k0 ** 2 * 2.5 ** 2, tol=.001)
-
-idx = lams.argsort()[::-1]
-lams = lams[idx]
-xs = xs[:, idx]
-xs = np.real(xs)
-
 if __name__ == "__main__":
     print([lam for lam in np.sort(np.sqrt(np.real(lams)) / k0) if lam > 0])
     # ~2.5 is the expected effective refractive index of the mode
 
     idx = 0
+    xs = np.real(xs)
     (Et, Etbasis), (Ez, Ezbasis), *_ = basis.split(xs[:, idx])
     print(np.sqrt(np.real(lams[idx])) / k0, np.sqrt(lams[idx]) / k0)
     print(np.sum(np.abs(Et)), np.sum(np.abs(Ez)))
@@ -88,7 +75,7 @@ if __name__ == "__main__":
     Etbasis.plot(Et).show()
     Ezbasis.plot(Ez, colorbar=True).show()
 
-    plot_basis = Etbasis.with_element(ElementVector(ElementDG(ElementTriP1())))
+    plot_basis = Etbasis.with_element(ElementVector(ElementTriP0()))
     Etp = plot_basis.project(Etbasis.interpolate(Et))
     (Etpx, plotx_basis), (Etpy, ploty_basis) = plot_basis.split(Etp)
 
