f185218abcc586c9ae94b70534f7667cbc399ebf
HelgeGehring <HelgeGehring@gmail.com>
2022-10-26 18:26:36 -0700

    switched to PETSc, seems now to find the right modes

--- mode_solver_2.py
+++ mode_solver_2.py
@@ -8,7 +8,7 @@ from skfem import *
 from skfem.helpers import *
 
 mesh = skfem.Mesh.load('mesh.msh')
-basis = Basis(mesh, ElementTriN0() * ElementTriP1())  # * ElementTriP1())
+basis = Basis(mesh, ElementTriN0() * ElementTriP1())
 
 basis0 = basis.with_element(ElementTriP0())
 epsilon = basis0.zeros()
@@ -22,41 +22,59 @@ one_over_u_r = 1
 
 
 @BilinearForm
-# def aform(E_t, E_z, lam, v_t, v_z, mu, w):
 def aform(E_t, E_z, v_t, v_z, w):
-    return one_over_u_r * curl(E_t) * curl(v_t) - k0 ** 2 * w['epsilon'] * dot(E_t, v_t) - one_over_u_r * dot(grad(E_z), v_t) + w['epsilon'] * inner(E_t, grad(v_z)) + w['epsilon'] * E_z*v_z
-
-
-# @BilinearForm
-# def gauge(E_t, E_z, lam, v_t, v_z, mu, w):
-# set div E = 0 using a Lagrange multiplier
-#    return dot(grad(lam), v_t) + dot(E_t, grad(mu))
+    return one_over_u_r * curl(E_t) * curl(v_t) - k0 ** 2 * w['epsilon'] * dot(E_t, v_t) - one_over_u_r * dot(grad(E_z),
+                                                                                                              v_t) + w[
+               'epsilon'] * inner(E_t, grad(v_z)) + w['epsilon'] * E_z * v_z
 
 
 @BilinearForm
-# def bform(E_t, E_z, lam, v_t, v_z, mu, w):
 def bform(E_t, E_z, v_t, v_z, w):
     return - one_over_u_r * dot(E_t, v_t)
 
 
 A = aform.assemble(basis, epsilon=basis0.interpolate(epsilon))
 B = bform.assemble(basis, epsilon=basis0.interpolate(epsilon))
-# C = gauge.assemble(basis, epsilon=basis0.interpolate(epsilon))
 
 # lams, xs = solve(*condense(A, B, D=basis.get_dofs()),
 #                 solver=solver_eigen_scipy_sym(k=10, sigma=k0 ** 2 * 2.5 ** 2))
+from petsc4py import PETSc
+from slepc4py import SLEPc
 
-lams, xs = scipy.linalg.eig(A.todense(), B.todense())
+A_ = PETSc.Mat().createAIJ(size=A.shape,
+                           csr=(A.indptr, A.indices,
+                                A.data))
+
+B_ = PETSc.Mat().createAIJ(size=B.shape,
+                           csr=(B.indptr, B.indices,
+                                B.data))
+
+eps = SLEPc.EPS().create()
+eps.setOperators(A_, B_)
+eps.setType(SLEPc.EPS.Type.KRYLOVSCHUR)
+st = eps.getST()
+st.setType(SLEPc.ST.Type.SINVERT)
+eps.setWhichEigenpairs(SLEPc.EPS.Which.TARGET_MAGNITUDE)
+eps.setTarget(100)
+eps.setDimensions(20)
+eps.solve()
+
+xr, xi = A_.getVecs()
+lams, xs = [], []
+for i in range(eps.getConverged()):
+    val = eps.getEigenpair(i, xr, xi)
+    lams.append(val)
+    xs.append(np.array(xr) + 1j * np.array(xi))
+lams = np.array(lams)
+xs = np.array(xs).T
+
+# lams, xs = scipy.linalg.eig(A.todense(), B.todense())
 # lams, xs = scipy.sparse.linalg.eigs(A, M=B, which='LR', sigma=k0 ** 2 * 2.5 ** 2, tol=.001)
 
-
 idx = lams.argsort()[::-1]
-#idx = idx[np.isfinite(lams[idx])]
 lams = lams[idx]
 xs = xs[:, idx]
-xs = xs.astype(float)
-print(lams.tolist())
-print(np.sqrt(np.real(lams))/k0)
+xs = np.real(xs)
 
 if __name__ == "__main__":
     print([lam for lam in np.sort(np.sqrt(np.real(lams)) / k0) if lam > 0])
@@ -64,13 +82,13 @@ if __name__ == "__main__":
 
     idx = 0
     (Et, Etbasis), (Ez, Ezbasis), *_ = basis.split(xs[:, idx])
-    print(np.sqrt(np.real(lams[idx]))/k0, np.sqrt(lams[idx])/k0)
+    print(np.sqrt(np.real(lams[idx])) / k0, np.sqrt(lams[idx]) / k0)
     print(np.sum(np.abs(Et)), np.sum(np.abs(Ez)))
 
     Etbasis.plot(Et).show()
     Ezbasis.plot(Ez, colorbar=True).show()
 
-    plot_basis = Etbasis.with_element(ElementVector(ElementTriP0()))
+    plot_basis = Etbasis.with_element(ElementVector(ElementDG(ElementTriP1())))
     Etp = plot_basis.project(Etbasis.interpolate(Et))
     (Etpx, plotx_basis), (Etpy, ploty_basis) = plot_basis.split(Etp)
 
