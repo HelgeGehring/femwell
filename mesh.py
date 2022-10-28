cf37d609ed6dc76e3ddd96c886c27e9818339cad
simbilod <sb30@princeton.edu>
2022-10-28 12:05:58 -0700

    mesh refinement works 90%

--- mesh.py
+++ mesh.py
@@ -5,7 +5,7 @@ import pygmsh
 import gmsh
 import shapely
 from shapely.geometry import Point, LineString, Polygon
-from shapely.ops import split
+from shapely.ops import split, triangulate
 
 from collections import OrderedDict
 
@@ -15,12 +15,16 @@ class MeshTracker():
         '''
         Map between shapely and gmsh 
         Shapely is useful for built-in geometry equivalencies and extracting orientation, instead of doing it manually
+        We can also track information about the entities using labels (useful for selective mesh refinement later)
         '''
         self.shapely_points = []
         self.gmsh_points = []
+        self.points_labels = []
         self.shapely_xy_lines = []
         self.gmsh_xy_lines = []
+        self.xy_lines_labels = []
         self.gmsh_xy_surfaces = []
+        self.xy_surfaces_labels = []
         self.model = model
         self.atol = atol
 
@@ -45,13 +49,34 @@ class MeshTracker():
                     return index, False
         return None, 1
 
+    def get_gmsh_points_from_label(self, label):
+        indices = [idx for idx, value in enumerate(self.points_labels) if value == label]
+        entities = []
+        for index in indices:
+            entities.append(self.gmsh_points[index]._id)
+        return entities
+
+    def get_gmsh_xy_lines_from_label(self, label):
+        indices = [idx for idx, value in enumerate(self.xy_lines_labels) if value == label]
+        entities = []
+        for index in indices:
+            entities.append(self.gmsh_xy_lines[index]._id)
+        return entities
+
+    def get_gmsh_xy_surfaces_from_label(self, label):
+        indices = [idx for idx, value in enumerate(self.xy_surfaces_labels) if value == label]
+        entities = []
+        for index in indices:
+            entities.append(self.gmsh_xy_surfaces[index]._id)
+        return entities
+
     """
     Channel loop utilities (no need to track)
     """
-    def xy_channel_loop_from_vertices(self, vertices):
+    def xy_channel_loop_from_vertices(self, vertices, label):
         edges = []
         for vertex1, vertex2 in [(vertices[i], vertices[i + 1]) for i in range(0, len(vertices)-1)]:
-            gmsh_line, orientation = self.add_get_xy_line(vertex1, vertex2)
+            gmsh_line, orientation = self.add_get_xy_line(vertex1, vertex2, label)
             if orientation:
                 edges.append(gmsh_line)
             else:
@@ -62,7 +87,7 @@ class MeshTracker():
     """
     Adding geometry
     """
-    def add_get_point(self, shapely_xy_point, resolution=None):
+    def add_get_point(self, shapely_xy_point, label=None):
         """
         Add a shapely point to the gmsh model, or retrieve the existing gmsh model points with equivalent coordinates (within tol.)
 
@@ -74,16 +99,14 @@ class MeshTracker():
         if index is not None:
             gmsh_point = self.gmsh_points[index]
         else:
-            if resolution is not None:
-                gmsh_point = self.model.add_point([shapely_xy_point.x, shapely_xy_point.y], mesh_size=resolution)
-            else:
-                gmsh_point = self.model.add_point([shapely_xy_point.x, shapely_xy_point.y])
+            gmsh_point = self.model.add_point([shapely_xy_point.x, shapely_xy_point.y])
             self.shapely_points.append(shapely_xy_point)
             self.gmsh_points.append(gmsh_point)
+            self.points_labels.append(label)
         return gmsh_point
 
 
-    def add_get_xy_line(self, shapely_xy_point1, shapely_xy_point2):
+    def add_get_xy_line(self, shapely_xy_point1, shapely_xy_point2, label):
         """
         Add a shapely line to the gmsh model in the xy plane, or retrieve the existing gmsh model line with equivalent coordinates (within tol.)
 
@@ -98,12 +121,12 @@ class MeshTracker():
             gmsh_line = self.model.add_line(self.add_get_point(shapely_xy_point1), self.add_get_point(shapely_xy_point2))
             self.shapely_xy_lines.append(shapely.geometry.LineString([shapely_xy_point1, shapely_xy_point2]))
             self.gmsh_xy_lines.append(gmsh_line)
+            self.xy_lines_labels.append(label)
         return gmsh_line, orientation
 
-    def add_xy_surface(self, shapely_xy_polygon, resolution=None):
+    def add_xy_surface(self, shapely_xy_polygon, label=None):
         """
         Add a xy surface corresponding to shapely_xy_polygon, or retrieve the existing gmsh model surface with equivalent coordinates (within tol.)
-        Note that there will never be collisions at this level, so no need to track
 
         Args:
             shapely_xy_polygon (shapely.geometry.Polygon):
@@ -116,19 +139,21 @@ class MeshTracker():
         for polygon_hole in list(shapely_xy_polygon.interiors):
             hole_vertices = []
             for vertex in shapely.geometry.MultiPoint(polygon_hole.coords):
-                gmsh_point = self.add_get_point(vertex, resolution=resolution)
+                gmsh_point = self.add_get_point(vertex, label)
                 hole_vertices.append(vertex)
-            hole_loops.append(self.xy_channel_loop_from_vertices(hole_vertices))
+            hole_loops.append(self.xy_channel_loop_from_vertices(hole_vertices, label))
         # Parse boundary
         for vertex in shapely.geometry.MultiPoint(shapely_xy_polygon.exterior.coords):
-            gmsh_point = self.add_get_point(vertex, resolution=resolution)
+            gmsh_point = self.add_get_point(vertex, label)
             exterior_vertices.append(vertex)
-        channel_loop = self.xy_channel_loop_from_vertices(exterior_vertices)
+        channel_loop = self.xy_channel_loop_from_vertices(exterior_vertices, label)
 
-        # Create surface
-        plane_surface = self.model.add_plane_surface(channel_loop, holes=hole_loops)
+        # Create and log surface
+        gmsh_surface = self.model.add_plane_surface(channel_loop, holes=hole_loops)
+        self.gmsh_xy_surfaces.append(gmsh_surface)
+        self.xy_surfaces_labels.append(label)
 
-        return plane_surface
+        return gmsh_surface
 
 
 def mesh_from_polygons(
@@ -160,15 +185,49 @@ def mesh_from_polygons(
     # Add surfaces, reusing lines to simplify at early stage
     meshtracker = MeshTracker(model=model)
     for polygon_name, polygon in reversed(polygons_tiled_dict.items()):
-        if polygon_name in resolutions.keys():
-            resolution = resolutions[polygon_name]
-        else:
-            resolution = None
-        plane_surface = meshtracker.add_xy_surface(polygon, resolution=resolution)
+        plane_surface = meshtracker.add_xy_surface(polygon, polygon_name)
         model.add_physical(plane_surface, f"{polygon_name}")
 
     # Remove duplicated lines to clean up partially overlapping edges
+    gmsh.model.occ.synchronize()
     gmsh.model.occ.removeAllDuplicates()
+    gmsh.model.occ.synchronize()
+
+    # Refinement in surfaces
+    n = 0
+    refinement_fields = []
+    for label, resolution in resolutions.items():
+        # Inside surface
+        mesh_resolution = resolution["resolution"]
+        gmsh.model.mesh.field.add("MathEval", n)
+        gmsh.model.mesh.field.setString(n, "F", f"{mesh_resolution}")
+        gmsh.model.mesh.field.add("Restrict", n+1)
+        gmsh.model.mesh.field.setNumber(n+1, "InField", n)
+        gmsh.model.mesh.field.setNumbers(n+1, "SurfacesList", meshtracker.get_gmsh_xy_surfaces_from_label(label))
+        # Around surface
+        mesh_distance = resolution["distance"]
+        gmsh.model.mesh.field.add("Distance", n+2)
+        gmsh.model.mesh.field.setNumbers(n+2, "CurvesList", meshtracker.get_gmsh_xy_lines_from_label(label))
+        gmsh.model.mesh.field.setNumber(n+2, "Sampling", 100)
+        gmsh.model.mesh.field.add("Threshold", n+3)
+        gmsh.model.mesh.field.setNumber(n+3, "InField", n+2)
+        gmsh.model.mesh.field.setNumber(n+3, "SizeMin", mesh_resolution)
+        gmsh.model.mesh.field.setNumber(n+3, "SizeMax", default_resolution_max)
+        gmsh.model.mesh.field.setNumber(n+3, "DistMin", 0)
+        gmsh.model.mesh.field.setNumber(n+3, "DistMax", mesh_distance)
+        # Save and increment
+        refinement_fields.append(n+1)
+        refinement_fields.append(n+3)
+        n += 4
+
+    # Use the smallest element size overall
+    gmsh.model.mesh.field.add("Min", n)
+    gmsh.model.mesh.field.setNumbers(n, "FieldsList", refinement_fields)
+    gmsh.model.mesh.field.setAsBackgroundMesh(n)
+
+    gmsh.model.mesh.MeshSizeFromPoints = 0
+    gmsh.model.mesh.MeshSizeFromCurvature = 0
+    gmsh.model.mesh.MeshSizeExtendFromBoundary = 0
 
     # Extract all unique lines (TODO: identify interfaces in label)
     i = 0
@@ -189,6 +248,7 @@ if __name__ == "__main__":
     hclad = 2
     hbox = 2
     offset_core = -0.1
+    offset_core2 = 1
     wcore = 0.5
     hcore = 0.22
     core = Polygon([
@@ -196,6 +256,12 @@ if __name__ == "__main__":
             Point(-wcore/2, hcore/2 + offset_core),
             Point(wcore/2, hcore/2 + offset_core),
             Point(wcore/2, -hcore/2 + offset_core),
+        ])
+    core2 = Polygon([
+            Point(-wcore/2, -hcore/2 + offset_core2),
+            Point(-wcore/2, hcore/2 + offset_core2),
+            Point(wcore/2, hcore/2 + offset_core2),
+            Point(wcore/2, -hcore/2 + offset_core2),
         ])
     clad = Polygon([
             Point(-wsim/2, -hcore/2),
@@ -212,12 +278,14 @@ if __name__ == "__main__":
 
     polygons = OrderedDict()
     polygons["core"] = core 
+    polygons["core2"] = core2
     polygons["clad"] = clad
     polygons["box"] = box
 
     resolutions = {}
-    resolutions["core"] = 0.01 
-    resolutions["clad"] = 0.2
+    resolutions["core"] = {"resolution": 0.02, "distance": 0.5}
+    resolutions["core2"] = {"resolution": 0.01, "distance": 0.5}
+    # resolutions["clad"] = {"resolution": 0.1, "dist_min": 0.01, "dist_max": 0.3}
 
 
     mesh = mesh_from_polygons(polygons, resolutions)
