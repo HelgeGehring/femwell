2da903b0ecb29d75d97376707a8b78f0fb181311
HelgeGehring <42973196+HelgeGehring@users.noreply.github.com>
2022-11-11 18:18:38 -0800

    start 1d mode solver

---
+++ femwell/mode_solver_1d.py
@@ -0,0 +1,39 @@
+import scipy.linalg
+
+from skfem import *
+from skfem.helpers import *
+
+mesh = MeshLine(np.linspace(-5, 5, 201))
+mesh = mesh.with_subdomains({'core': lambda p: abs(p[0]) < .22})
+
+basis_epsilon = Basis(mesh, ElementLineP0())
+
+epsilon = basis_epsilon.zeros() + 1.444 ** 2
+epsilon[basis_epsilon.get_dofs(elements='core')] = 3.4777 ** 2
+basis_epsilon.plot(epsilon).show()
+
+wavelength = 1.55
+k0 = 2 * np.pi / wavelength
+
+
+@BilinearForm
+def lhs(u, v, w):
+    return 1 / k0 ** 2 * dot(grad(u), grad(v)) + w['epsilon'] * inner(u, v)
+
+
+@BilinearForm
+def rhs(u, v, w):
+    return inner(u, v)
+
+
+basis = basis_epsilon.with_element(ElementLineP1())
+
+A = lhs.assemble(basis, epsilon=basis_epsilon.interpolate(epsilon))
+B = rhs.assemble(basis)
+
+lams, xs = scipy.linalg.eigh(A.todense(), B.todense())
+# lams, xs = solve(*condense(A, B, D=basis.get_dofs()), solver=solver_eigen_scipy(sigma=3.55 ** 2, which='LR'))
+
+print(lams)
+basis.plot(xs[:, -1]).show()
+basis.plot(np.abs(xs[:, -1])).show()
