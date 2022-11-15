80fde10dd4c34dffc0fcb5ee7fde87d8d9b4cb73
HelgeGehring <42973196+HelgeGehring@users.noreply.github.com>
2022-11-14 21:00:31 -0800

    started overlaps with fibers

---
+++ femwell/examples/fiber_overlap.py
@@ -0,0 +1,79 @@
+import tempfile
+
+from collections import OrderedDict
+
+import matplotlib.pyplot as plt
+import numpy as np
+import shapely.geometry
+
+from skfem import Mesh, Basis, ElementTriP0, ElementTriP1, Functional
+
+from femwell.mode_solver import compute_modes, plot_mode
+from femwell.mesh import mesh_from_polygons
+
+
+def zr(mfr, refractive_index, wavelength):
+    return np.pi * mfr ** 2 * refractive_index / wavelength
+
+
+def r_at(z, mfr, refractive_index, wavelength):
+    if z == 0:
+        return np.inf
+    return z * (1 + z / zr(mfr, refractive_index, wavelength))
+
+
+def mfr_at(mfr, z, refractive_index, wavelength):
+    return mfr * np.sqrt(1 + z / zr(mfr, refractive_index, wavelength))
+
+
+def e_field_gaussian(r, z, mfr, refractive_index, wavelength):
+    mfr_at_z = mfr_at(mfr, z, refractive_index, wavelength)
+    k = 2 * np.pi * refractive_index / wavelength
+
+    return (
+            1 / mfr_at_z / np.sqrt(np.pi / 2)
+            * np.exp(-r ** 2 / mfr_at_z ** 2)
+        # * np.exp(-1j * k * z + k * r ** 2 / (2 * r_at(z, mfr, refractive_index, wavelength)))
+    )
+
+
+if __name__ == '__main__':
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        core = shapely.geometry.box(-.2, -.17, .2, .17)
+
+        polygons = OrderedDict(
+            core=core,
+            clad=core.buffer(10)
+        )
+
+        resolutions = dict(
+            core={"resolution": .1, "distance": 2}
+        )
+
+        mesh_from_polygons(polygons, resolutions, filename='mesh.msh', default_resolution_max=10)
+        mesh_from_polygons(polygons, resolutions, filename=tmpdirname + '/mesh.msh', default_resolution_max=10)
+        mesh = Mesh.load(tmpdirname + '/mesh.msh')
+
+    basis0 = Basis(mesh, ElementTriP0(), intorder=4)
+    epsilon = basis0.zeros().astype(complex)
+    epsilon[basis0.get_dofs(elements='core')] = 2 ** 2
+    epsilon[basis0.get_dofs(elements='clad')] = 1.444 ** 2
+    basis0.plot(np.real(epsilon), colorbar=True).show()
+
+    # lams, basis, xs = compute_modes(basis0, epsilon, wavelength=1.55, mu_r=1, num_modes=1)
+
+    # fig, axs = plot_mode(basis, np.real(xs[0]), direction='x')
+    # plt.show()
+
+    basis = basis0.with_element(ElementTriP1())
+    x_fiber = basis.project(lambda x: e_field_gaussian(np.sqrt(x[0] ** 2 + x[1] ** 2), 0, 9 / 2, 1, 1.55))
+
+    basis.plot(np.real(x_fiber)).show()
+
+
+    @Functional(dtype=np.complex64)
+    def overlap_integral(w):
+        return w['E_i'] * w['E_j']
+
+
+    print(overlap_integral.assemble(basis, E_i=basis.interpolate(x_fiber), E_j=basis.interpolate(x_fiber)))
