876b38571059f2f1d147aa909b07dae827483611
HelgeGehring <42973196+HelgeGehring@users.noreply.github.com>
2022-11-17 23:44:06 -0800

    start adding a mode solver

--- femwell/fefd.py
+++ femwell/fefd.py
@@ -1,11 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-from skfem import BilinearForm, Basis, ElementTriN1, ElementTriP0, ElementTriP1, Mesh, solve, FacetBasis
+from skfem import BilinearForm, Basis, ElementTriN1, ElementTriP0, ElementTriP1, Mesh, solve, FacetBasis, condense, \
+    InteriorFacetBasis
 from skfem.helpers import curl, grad, dot, inner
 
 
-def compute_modes(basis, basis_epsilon_r, epsilon_r, wavelength, mu_r, source):
+def compute_modes(basis, basis_epsilon_r, epsilon_r, wavelength, mu_r, source, D=None, x0=None):
     k0 = 2 * np.pi / wavelength
     one_over_u_r = 1 / mu_r
 
@@ -21,7 +22,12 @@ def compute_modes(basis, basis_epsilon_r, epsilon_r, wavelength, mu_r, source):
     A = curl_form.assemble(basis, epsilon=basis_epsilon_r.interpolate(epsilon_r))
     C = div_form.assemble(basis)
 
-    return basis, solve(A + C, source)
+    if x0 is None:
+        x = solve(A + C, source)
+    else:
+        x = solve(*condense(A + C, source, D=D, x=x0))
+
+    return basis, x
 
 
 if __name__ == '__main__':
@@ -58,8 +64,8 @@ if __name__ == '__main__':
     ])
 
     source = LineString([
-        (width_wg_1 / 2, -length_wg_1 / 2),
-        (-width_wg_1 / 2, -length_wg_1 / 2)
+        (width_wg_2 / 2, -length_wg_1 / 2),
+        (-width_wg_2 / 2, -length_wg_1 / 2)
     ])
 
     polygons = OrderedDict(
@@ -88,14 +94,43 @@ if __name__ == '__main__':
     epsilon[basis0.get_dofs(elements='pml')] = (1.444 + 1j) ** 2
     basis0.plot(np.real(epsilon)).show()
 
-    basis_source = FacetBasis(mesh, basis.elem, facets=mesh.boundaries['core___core_append'])
+    basis0_source = FacetBasis(mesh, basis0.elem, facets=mesh.boundaries['source'], intorder=4)
+    basis_source = FacetBasis(mesh, basis.elem, facets=mesh.boundaries['source'], intorder=4)
 
-    source = basis_source.project(
-        lambda x: [np.array([0 + 0 * x[0], 0 * np.exp(0 * x[0] ** 2) + 0 * x[0]]), 1 + 0 * x[0]])
-    source = source.astype(complex)
-    source *= 1j
+    # source = basis_source.project(
+    #    lambda x: [np.array([0 + 0 * x[0], 0 * np.exp(0 * x[0] ** 2) + 0 * x[0]]), 1 + 0 * x[0]])
+    # source = source.astype(complex)
+    # source *= 1j
 
-    basis, x = compute_modes(basis, basis0, epsilon, 1.55, 1, source)
+    basis_source_1d = basis_source.with_element(ElementTriP1())
+
+    wavelength = 1.55
+    k0 = 2 * np.pi / wavelength
+
+
+    @BilinearForm
+    def lhs(u, v, w):
+        return -1 / k0 ** 2 * inner(grad(u)[0], grad(v)[0]) + w['epsilon'] * inner(u, v)
+
+
+    @BilinearForm
+    def rhs(u, v, w):
+        return inner(u, v)
+
+    A = lhs.assemble(basis_source_1d, epsilon=basis0_source.interpolate(epsilon))
+    B = rhs.assemble(basis_source_1d)
+
+    from skfem.utils import solver_eigen_scipy_sym
+
+    lams, xs = solve(*condense(A, B, I=basis_source_1d.get_dofs(facets='source')),
+                     solver=solver_eigen_scipy_sym(sigma=3.55 ** 2, which='LM'))
+    print(np.sqrt(lams))
+
+    # xs[:,0] = xs[:,0]*0+1
+    x0 = basis_source.project((np.array((basis_source_1d.interpolate(0*xs[:,-1]), basis_source_1d.interpolate(xs[:,-1]))), basis_source_1d.interpolate(0*xs[:,-1])))
+
+    basis, x = compute_modes(basis, basis0, epsilon, 1.55, 1, basis.zeros(), D=basis_source.get_dofs(facets='source'),
+                             x0=x0)
 
     from mode_solver import plot_mode
 
