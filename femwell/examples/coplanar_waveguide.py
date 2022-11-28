a8f7d4487a61e1e307986f5edb061a3cdc9b54ad
HelgeGehring <42973196+HelgeGehring@users.noreply.github.com>
2022-11-28 12:03:48 -0800

    add example with only one waveguide

--- femwell/examples/coplanar_waveguide.py
+++ femwell/examples/coplanar_waveguide.py
@@ -14,7 +14,33 @@ from femwell.mode_solver import compute_modes, plot_mode, calculate_hfield
 from femwell.mesh import mesh_from_OrderedDict
 
 
-def mesh_waveguide(filename, wsim, hclad, hsi, wcore, hcore, gap):
+def mesh_waveguide(filename, wsim, hclad, hsi, wcore, hcore):
+    core = box(-wcore / 2, -hcore / 2, wcore / 2, hcore / 2)
+    clad = box(-wsim / 2, -hcore / 2, wsim / 2, -hcore / 2 + hclad)
+    silicon = box(-wsim / 2, -hcore / 2, wsim / 2, -hcore / 2 - hsi)
+
+    all = shapely.ops.unary_union([core, clad, silicon])
+
+    polygons = OrderedDict(
+        boundary=LineString(all.exterior),
+        interface=LineString([
+            (-wsim / 2, -hcore / 2),
+            (wsim / 2, -hcore / 2),
+        ]),
+        core_interface=core.exterior,
+        core=core,
+        clad=clad,
+        silicon=silicon,
+    )
+
+    resolutions = dict(
+        core_interface={"resolution": .003, "distance": 1},
+    )
+
+    return mesh_from_OrderedDict(polygons, resolutions, filename=filename, default_resolution_max=1)
+
+
+def mesh_waveguide_1(filename, wsim, hclad, hsi, wcore, hcore, gap):
     core_l = box(-wcore - gap, -hcore / 2, -gap / 2, hcore / 2)
     core_r = box(wcore + gap, -hcore / 2, gap / 2, hcore / 2)
     clad = box(-wsim / 2, -hcore / 2, wsim / 2, -hcore / 2 + hclad)
@@ -37,32 +63,36 @@ def mesh_waveguide(filename, wsim, hclad, hsi, wcore, hcore, gap):
     )
 
     resolutions = dict(
-        interface={"resolution": .01, "distance": 1},
+        core_l_interface={"resolution": .003, "distance": 1},
+        core_r_interface={"resolution": .003, "distance": 1},
     )
 
     return mesh_from_OrderedDict(polygons, resolutions, filename=filename, default_resolution_max=1)
 
 
 if __name__ == '__main__':
-    omega = 9e-6
-    print('lambda ', 2 * np.pi / omega / scipy.constants.c  * .1277)
+    omega = 9e9
+    print('lambda ', 2 * np.pi * scipy.constants.c * 1e3 / omega)
 
     with tempfile.TemporaryDirectory() as tmpdirname:
-        mesh_waveguide(wsim=10, hclad=4, hsi=1, wcore=2, hcore=.02, gap=1,
+        mesh_waveguide_1(wsim=10, hclad=4, hsi=1, wcore=2, hcore=.02, gap=1,
                        filename=tmpdirname + '/mesh.msh')
         mesh = Mesh.load(tmpdirname + '/mesh.msh')
 
     basis0 = Basis(mesh, ElementTriP0(), intorder=4)
     epsilon = basis0.zeros().astype(complex)
     epsilon[basis0.get_dofs(elements='clad')] = 1
-    epsilon[basis0.get_dofs(elements='core_r')] = 1 + 1j * 5.8e7 / omega
-    epsilon[basis0.get_dofs(elements='core_l')] = 1 + 1j * 5.8e7 / omega
+    # epsilon[basis0.get_dofs(elements='core')] = + 1j * 5.8e7 * 1e20 / omega
+    epsilon[basis0.get_dofs(elements='core_l')] = + 1j * 5.8e7 * 1e20 / omega
+    epsilon[basis0.get_dofs(elements='core_r')] = + 1j * 5.8e7 * 1e20 / omega
     epsilon[basis0.get_dofs(elements='silicon')] = 4
     basis0.plot(np.real(epsilon), colorbar=True).show()
 
+    print(2 * np.pi * scipy.constants.c * 1e3 / omega, '<--')
     print(2 * np.pi / omega)
     conductors = ['core_l_interface', 'core_r_interface']
-    lams, basis, xs = compute_modes(basis0, epsilon, wavelength=2 * np.pi / omega, mu_r=1, num_modes=len(conductors))
+    lams, basis, xs = compute_modes(basis0, epsilon, wavelength=1/.1, mu_r=1,
+                                    num_modes=len(conductors))
     print('lams', lams)
 
     fig, axs = plot_mode(basis, np.real(xs[1]), plot_vectors=True)
@@ -90,6 +120,9 @@ if __name__ == '__main__':
             current = abs(current_form.assemble(facet_basis, H=facet_basis.interpolate(ht)))
             print(f'mode {mode_i} current in ' + conductor + '\t', current)
             currents[conductors_i, mode_i] = current
+
+    print('currents', currents)
+    print(np.linalg.inv(currents))
 
     characteristic_impedances = np.linalg.inv(currents).T @ np.linalg.inv(currents)
     print(characteristic_impedances)
