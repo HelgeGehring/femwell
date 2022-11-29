5c7cf9b4b32957412352d00ca51288d167e09f47
HelgeGehring <42973196+HelgeGehring@users.noreply.github.com>
2022-11-28 17:22:12 -0800

    use more realistic example

--- femwell/culomb.py
+++ femwell/culomb.py
@@ -31,54 +31,41 @@ if __name__ == '__main__':
 
     from collections import OrderedDict
 
-    electrode_left = box(9, 1, 10, 1.2)
-    electrode_right = box(-9, 1, -10, 1.2)
-    core = box(-10, 0, 10, 1)
-    env = core.buffer(15, resolution=8)
+    electrode_left = box(9, .15, 10, .2)
+    electrode_right = box(-9, .15, -10, .2)
+    slab = box(-10, 0, 10, .15)
+    core = box(-.5, .15, .5, .3)
+    env = slab.buffer(20, resolution=8)
 
     polygons = OrderedDict(
         electrode_left=electrode_left,
         electrode_right=electrode_right,
+        slab=slab,
         core=core,
         env=env
     )
 
     resolutions = dict(
-        core={"resolution": .1, "distance": .5}
+        slab={"resolution": .02, "distance": .1},
+        core={"resolution": .02, "distance": .1}
     )
 
     with tempfile.TemporaryDirectory() as tmpdirname:
-        mesh_from_OrderedDict(polygons, resolutions, filename=tmpdirname + '/mesh.msh', default_resolution_max=2)
-        mesh_from_OrderedDict(polygons, resolutions, filename='mesh.msh', default_resolution_max=2)
+        mesh_from_OrderedDict(polygons, resolutions, filename=tmpdirname + '/mesh.msh', default_resolution_max=5)
+        mesh_from_OrderedDict(polygons, resolutions, filename='mesh.msh', default_resolution_max=5)
         mesh = Mesh.load(tmpdirname + '/mesh.msh')
 
     basis = Basis(mesh, ElementTriP0())
 
     epsilon = basis.ones()
+    epsilon[basis.get_dofs(elements='slab')] = 1000
     epsilon[basis.get_dofs(elements='core')] = 1000
     # basis.plot(epsilon).show()
 
-    basis_u, u = solve_coulomb(basis, epsilon, {'electrode_left___core': 1, 'electrode_right___core': 0})
+    basis_u, u = solve_coulomb(basis, epsilon, {'electrode_left___slab': 1, 'electrode_right___slab': 0})
 
     basis_vec = basis_u.with_element(ElementVector(ElementTriP1()))
     u_grad = basis_vec.project(basis.interpolate(epsilon) * basis_u.interpolate(u).grad)
-
-    x = np.linspace(np.min(basis.mesh.p[0]), np.max(basis.mesh.p[0]), 100)
-    y = np.linspace(np.min(basis.mesh.p[1]), np.max(basis.mesh.p[1]), 100)
-
-    stacked = np.dstack(np.meshgrid(x, y))
-    reshaped = stacked.reshape(-1, 2)
-
-    from shapely.prepared import prep
-
-    env_preped = prep(env)
-    inside = np.array([env_preped.contains(Point(point)) for point in reshaped])
-    (u_grad_x, _), (u_grad_y, _) = basis_vec.split(u_grad)
-    result = np.full_like(reshaped, np.nan)
-    result[inside] = np.array(
-        (basis_u.interpolator(u_grad_x)(reshaped[inside].T), basis_u.interpolator(u_grad_y)(reshaped[inside].T))).T
-    result = result.reshape(stacked.shape)
-
     fig, ax = plt.subplots()
 
     for subdomain in basis.mesh.subdomains.keys() - {'gmsh:bounding_entities'}:
@@ -86,13 +73,9 @@ if __name__ == '__main__':
 
     basis_u.plot(u, ax=ax, shading='gouraud', colorbar=True)
     # basis_vec.plot(-u_grad, ax=ax)
-    import matplotlib
+    plt.show()
 
-    c_white = matplotlib.colors.colorConverter.to_rgba('black', alpha=0.1)
-    c_black = matplotlib.colors.colorConverter.to_rgba('black', alpha=1)
-    cmap_rb = matplotlib.colors.LinearSegmentedColormap.from_list('rb_cmap', [c_white, c_black], 512)
-
-    plt.streamplot(x, y, result[..., 0], result[..., 1], color=(np.linalg.norm(result, axis=-1)), density=5,
-                   cmap=cmap_rb)
-    plt.colorbar()
+    basis_grad = basis_u.with_element(ElementDG(basis_u.elem))
+    basis_u.plot(basis_u.project(basis.interpolate(epsilon) * basis_u.interpolate(u).grad[0]), shading='gouraud',
+                 colorbar=True)
     plt.show()
