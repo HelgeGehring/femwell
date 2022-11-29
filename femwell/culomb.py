3be0041432fadd7b3fbe96c14364843467d5bf0a
HelgeGehring <42973196+HelgeGehring@users.noreply.github.com>
2022-11-28 18:14:06 -0800

    adjust parameters to reference paper

--- femwell/culomb.py
+++ femwell/culomb.py
@@ -25,16 +25,21 @@ def solve_coulomb(basis_epsilon_r, epsilon_r, fixed_boundaries):
 
 
 if __name__ == '__main__':
+    # Reproduce https://doi.org/10.3390/
+
     import tempfile
 
-    from shapely.geometry import box, Point
+    from shapely.geometry import box
 
     from collections import OrderedDict
 
-    electrode_left = box(9, .15, 10, .2)
-    electrode_right = box(-9, .15, -10, .2)
-    slab = box(-10, 0, 10, .15)
-    core = box(-.5, .15, .5, .3)
+    electrode_start_x = 1.532 / 2 + 2.629
+    electrode_width = 4.4
+
+    electrode_left = box(-electrode_start_x - electrode_width, .5, - electrode_start_x, .5 + 1.8)
+    electrode_right = box(electrode_start_x, .5, electrode_start_x + electrode_width, .5 + 1.8)
+    slab = box(-10, 0, 10, .5)
+    core = box(-1.532 / 2, .5, 1.532 / 2, .8)
     env = slab.buffer(20, resolution=8)
 
     polygons = OrderedDict(
@@ -55,27 +60,30 @@ if __name__ == '__main__':
         mesh_from_OrderedDict(polygons, resolutions, filename='mesh.msh', default_resolution_max=5)
         mesh = Mesh.load(tmpdirname + '/mesh.msh')
 
-    basis = Basis(mesh, ElementTriP0())
+    basis = Basis(mesh, ElementTriP1())
+    basis_epsilon_r = basis.with_element(ElementTriP0())
 
-    epsilon = basis.ones()
-    epsilon[basis.get_dofs(elements='slab')] = 1000
-    epsilon[basis.get_dofs(elements='core')] = 1000
+    epsilon = basis_epsilon_r.zeros() + 3.9
+    epsilon[basis_epsilon_r.get_dofs(elements='slab')] = 128.4
+    epsilon[basis_epsilon_r.get_dofs(elements='core')] = 7.5
     # basis.plot(epsilon).show()
 
-    basis_u, u = solve_coulomb(basis, epsilon, {'electrode_left___slab': 1, 'electrode_right___slab': 0})
+    basis_u, u = solve_coulomb(basis_epsilon_r, epsilon, {'electrode_left___slab': 1, 'electrode_right___slab': 0})
 
-    basis_vec = basis_u.with_element(ElementVector(ElementTriP1()))
-    u_grad = basis_vec.project(basis.interpolate(epsilon) * basis_u.interpolate(u).grad)
     fig, ax = plt.subplots()
 
-    for subdomain in basis.mesh.subdomains.keys() - {'gmsh:bounding_entities'}:
-        basis.mesh.restrict(subdomain).draw(ax=ax, boundaries_only=True)
+    for subdomain in basis_epsilon_r.mesh.subdomains.keys() - {'gmsh:bounding_entities'}:
+        basis_epsilon_r.mesh.restrict(subdomain).draw(ax=ax, boundaries_only=True)
 
     basis_u.plot(u, ax=ax, shading='gouraud', colorbar=True)
     # basis_vec.plot(-u_grad, ax=ax)
     plt.show()
 
+    fig, ax = plt.subplots()
+
+    for subdomain in basis_epsilon_r.mesh.subdomains.keys() - {'gmsh:bounding_entities'}:
+        basis_epsilon_r.mesh.restrict(subdomain).draw(ax=ax, boundaries_only=True)
     basis_grad = basis_u.with_element(ElementDG(basis_u.elem))
-    basis_u.plot(basis_u.project(basis.interpolate(epsilon) * basis_u.interpolate(u).grad[0]), shading='gouraud',
-                 colorbar=True)
+    basis_u.plot(basis_u.project(basis_epsilon_r.interpolate(epsilon) * basis_u.interpolate(u).grad[0]),
+                 ax=ax, shading='gouraud', colorbar=True)
     plt.show()
