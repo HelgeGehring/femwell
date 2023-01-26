daa44734d6d9d814e0761fe2d17ed28343fbc06e
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2023-01-26 12:23:16 -0800

    improve fiber_overlap notebooks

--- docs/photonics/examples/fiber_overlap.py
+++ docs/photonics/examples/fiber_overlap.py
@@ -13,28 +13,25 @@ from femwell.mesh import mesh_from_OrderedDict
 from femwell.fiber import e_field_gaussian
 
 
-core = shapely.geometry.box(-.1, -.15, .1, .15)
+core = shapely.geometry.box(-0.1, -0.15, 0.1, 0.15)
 
-polygons = OrderedDict(
-    core=core,
-    clad=core.buffer(15, resolution=4)
+polygons = OrderedDict(core=core, clad=core.buffer(15, resolution=4))
+
+resolutions = dict(core={"resolution": 0.01, "distance": 0.1})
+
+mesh = from_meshio(
+    mesh_from_OrderedDict(polygons, resolutions, default_resolution_max=10)
 )
-
-resolutions = dict(
-    core={"resolution": .01, "distance": .1}
-)
-
-mesh = from_meshio(mesh_from_OrderedDict(polygons, resolutions, default_resolution_max=10))
 
 basis0 = Basis(mesh, ElementTriP0(), intorder=4)
 epsilon = basis0.zeros().astype(complex)
-epsilon[basis0.get_dofs(elements='core')] = 1.9963 ** 2
-epsilon[basis0.get_dofs(elements='clad')] = 1.444 ** 2
+epsilon[basis0.get_dofs(elements="core")] = 1.9963**2
+epsilon[basis0.get_dofs(elements="clad")] = 1.444**2
 basis0.plot(np.real(epsilon), colorbar=True).show()
 
 lams, basis, xs = compute_modes(basis0, epsilon, wavelength=1.55, mu_r=1, num_modes=1)
 
-fig, axs = plot_mode(basis, np.real(xs[0]), direction='x')
+fig, axs = plot_mode(basis, np.real(xs[0]), direction="x")
 plt.show()
 
 mfds = np.linspace(2, 20, 100)
@@ -42,28 +39,35 @@ efficiencies = []
 
 for mfd in tqdm(mfds):
     basis_fiber = basis0.with_element(ElementTriP1())
-    x_fiber = basis_fiber.project(lambda x: e_field_gaussian(np.sqrt(x[0] ** 2 + x[1] ** 2), 0, mfd / 2, 1, 1.55),
-                                    dtype=np.cfloat)
-
+    x_fiber = basis_fiber.project(
+        lambda x: e_field_gaussian(np.sqrt(x[0] ** 2 + x[1] ** 2), 0, mfd / 2, 1, 1.55),
+        dtype=np.cfloat,
+    )
 
     # basis_fiber.plot(np.real(x_fiber)).show()
 
     @Functional(dtype=np.complex64)
     def overlap_integral(w):
-        return w['E_i'] * np.conj(w['E_j'])
+        return w["E_i"] * np.conj(w["E_j"])
 
-    efficiency = np.abs(overlap_integral.assemble(basis_fiber,
-                                                    E_i=basis.interpolate(xs[0])[0][1],
-                                                    E_j=basis_fiber.interpolate(x_fiber))
-                        /
-                        np.sqrt(
-                            overlap_integral.assemble(basis_fiber,
-                                                        E_i=basis.interpolate(xs[0])[0][1],
-                                                        E_j=basis.interpolate(xs[0])[0][1])
-                            )
-                        )
+    efficiency = np.abs(
+        overlap_integral.assemble(
+            basis_fiber,
+            E_i=basis.interpolate(xs[0])[0][1],
+            E_j=basis_fiber.interpolate(x_fiber),
+        )
+        / np.sqrt(
+            overlap_integral.assemble(
+                basis_fiber,
+                E_i=basis.interpolate(xs[0])[0][1],
+                E_j=basis.interpolate(xs[0])[0][1],
+            )
+        )
+    )
 
     efficiencies.append(efficiency)
 
 plt.plot(mfds, efficiencies)
+plt.xlabel("Mode field diameter (um)")
+plt.ylabel("Coupling efficiency")
 plt.show()
--- docs/photonics/examples/waveguide_modes.py
+++ docs/photonics/examples/waveguide_modes.py
@@ -11,26 +11,32 @@ from skfem.io.meshio import from_meshio
 from femwell.mode_solver import compute_modes, plot_mode
 from femwell.mesh import mesh_from_OrderedDict
 
-core = shapely.box(-.5, 0, .5, .33)
-env = shapely.affinity.scale(core.buffer(5, resolution=8), xfact=.5)
+wg_width = 0.2
+wg_thickness = 0.3
+core = shapely.geometry.box(
+    -wg_width / 2, -wg_thickness / 2, +wg_width / 2, +wg_thickness / 2
+)
+env = shapely.affinity.scale(core.buffer(5, resolution=8), xfact=0.5)
 
 polygons = OrderedDict(
     core=core,
     box=clip_by_rect(env, -np.inf, -np.inf, np.inf, 0),
-    clad=clip_by_rect(env, -np.inf, 0, np.inf, np.inf)
+    clad=clip_by_rect(env, -np.inf, 0, np.inf, np.inf),
 )
 
-resolutions = dict(
-    core={"resolution": .03, "distance": .1}
-)
+resolutions = dict(core={"resolution": 0.03, "distance": 0.1})
 
-mesh = from_meshio(mesh_from_OrderedDict(polygons, resolutions, default_resolution_max=10))
+mesh = from_meshio(
+    mesh_from_OrderedDict(polygons, resolutions, default_resolution_max=10)
+)
 
 basis0 = Basis(mesh, ElementTriP0())
 epsilon = basis0.zeros(dtype=complex)
-for subdomain, n in {'core': 1.9963, 'box': 1.444, 'clad': 1}.items():
+for subdomain, n in {"core": 1.9963, "box": 1.444, "clad": 1}.items():
     epsilon[basis0.get_dofs(elements=subdomain)] = n**2
 
-lams, basis, xs = compute_modes(basis0, epsilon, wavelength=1.55, mu_r=1, num_modes=1, order=2)
-plot_mode(basis, xs[0].real, colorbar=True, direction='x')
+lams, basis, xs = compute_modes(
+    basis0, epsilon, wavelength=1.55, mu_r=1, num_modes=1, order=2
+)
+plot_mode(basis, xs[0].real, colorbar=True, direction="x")
 plt.show()
