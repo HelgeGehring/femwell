eda4bfdf694db5a619ef2d5768f8a6ca19b3ed1a
HelgeGehring <42973196+HelgeGehring@users.noreply.github.com>
2023-02-09 18:14:21 -0800

    work on coupled mode theory

--- femwell/examples/coupled_mode_theory.py
+++ femwell/examples/coupled_mode_theory.py
@@ -24,8 +24,8 @@ w_sim = 4
 h_clad = 1
 h_box = 1
 w_core_1 = 0.45
-w_core_2 = 0.5
-gap = 0.5
+w_core_2 = 0.45
+gap = 0.2
 h_core = 0.22
 offset_heater = 2.2
 h_heater = 0.14
@@ -78,6 +78,20 @@ mesh_from_OrderedDict(polygons, resolutions, filename="mesh.msh", default_resolu
 
 mesh = Mesh.load("mesh.msh")
 basis0 = Basis(mesh, ElementTriP0(), intorder=4)
+
+epsilon = basis0.zeros()
+epsilon[basis0.get_dofs(elements="core_1")] = 3.4777**2
+epsilon[basis0.get_dofs(elements="core_2")] = 3.4777**2
+epsilon[basis0.get_dofs(elements="clad")] = 1.444**2
+epsilon[basis0.get_dofs(elements="box")] = 1.444**2
+# basis0.plot(epsilon, colorbar=True).show()
+
+lams_both, basis, xs_both = compute_modes(
+    basis0, epsilon, wavelength=wavelength, mu_r=1, num_modes=2
+)
+print(lams_both)
+print("coupling_length", 1 / (2 * np.pi / wavelength * (lams_both[0] - lams_both[1])) * np.pi)
+
 epsilon = basis0.zeros()
 epsilon[basis0.get_dofs(elements="core_1")] = 3.4777**2
 epsilon[basis0.get_dofs(elements="core_2")] = 1.444**2
@@ -174,37 +188,41 @@ beta_c = (kappas[0, 1] * kappas[1, 0] + delta**2) ** 0.5
 print(np.pi / (2 * beta_c))
 
 eta = np.abs(kappas[1, 0] ** 2 / beta_c**2) * np.sin(beta_c * 1e3)
-print(eta, np.abs(kappas[1, 0] ** 2 / beta_c**2))
+print("eta", eta, np.abs(kappas[1, 0] ** 2 / beta_c**2))
 
 # see http://home.iitj.ac.in/~k.r.hiremath/research/thesis.pdf , not yet finished
 
 
 def fun(t, y):
     phase_matrix = [
-        [np.exp(2j * np.pi / wavelength * (lam_i - lam_j) * t) for lam_j, E_j, epsilon_j in modes]
+        [
+            np.exp(2j * np.pi / wavelength * (lam_i - lam_j) * t / 5)
+            for lam_j, E_j, epsilon_j in modes
+        ]
         for lam_i, E_i, epsilon_i in modes
     ]
     matrix = (
         np.linalg.inv(overlap_integrals * phase_matrix)
-        @ coupling_coefficients
-        * phase_matrix
+        @ (coupling_coefficients * phase_matrix)
         * -1j
         * scipy.constants.speed_of_light
         * epsilon_0
     )
-    return (matrix @ y.reshape(matrix.shape)).ravel()
+    return (matrix @ y).ravel()
 
 
-stepping = RK45(fun, 0, np.eye(len(modes), dtype=complex).ravel(), 300, max_step=3)
+stepping = RK45(fun, 0, np.array((1, 0), dtype=complex), 200, max_step=1)
 
 ts = []
 ys = []
 
-for i in range(100):
+for i in range(200):
     stepping.step()
     ts.append(stepping.t)
     ys.append(stepping.y)
 
-plt.plot(ts, np.abs(np.array(ys).reshape((-1,) + coupling_coefficients.shape) @ (1, 0)) ** 2, "r")
+plt.plot(ts, np.abs(np.array(ys)[:, 0]) ** 2, "r")
+plt.plot(ts, np.abs(np.array(ys)[:, 1]) ** 2, "r")
+plt.plot(ts, np.sum(np.abs(np.array(ys)) ** 2, axis=-1), "r")
 # plt.plot(ts, np.array(ys).imag.reshape((-1,)+matrix.shape)@(1,0), 'g')
 plt.show()
