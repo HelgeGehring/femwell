e46056713db5ce5a01e3e8ef05b732d6afd97e85
HelgeGehring <42973196+HelgeGehring@users.noreply.github.com>
2023-02-20 22:20:53 -0800

    simplify example

--- docs/photonics/examples/leaky_mode.py
+++ docs/photonics/examples/leaky_mode.py
@@ -22,7 +22,7 @@ from collections import OrderedDict
 
 import matplotlib.pyplot as plt
 import numpy as np
-import shapely
+from shapely import LineString, box
 from skfem import Basis, ElementDG, ElementTriP1
 from skfem.io import from_meshio
 
@@ -41,15 +41,15 @@ pml = 3
 wavelength = 1
 k0 = 2 * np.pi / wavelength
 
-left = shapely.LineString([(0, y) for y in np.linspace(-height, height, 2)])
-right = shapely.LineString([(a, y) for y in np.linspace(-height, height, 2)])
-top = shapely.LineString([(x, height) for x in np.linspace(0, a, 2)])
-bottom = shapely.LineString([(x, -height) for x in np.linspace(0, a, 2)])
+left = LineString([(0, y) for y in np.linspace(-height, height, 2)])
+right = LineString([(a, y) for y in np.linspace(-height, height, 2)])
+top = LineString([(x, height) for x in np.linspace(0, a, 2)])
+bottom = LineString([(x, -height) for x in np.linspace(0, a, 2)])
 
-box = shapely.box(0, -height, a, height)
-structure = shapely.box(0, -b / 2, a, b / 2)
-structure1 = shapely.box(0, height - slab, a, height)
-structure2 = shapely.box(0, -height + slab, a, -height)
+background = box(0, -height, a, height)
+structure = box(0, -b / 2, a, b / 2)
+structure1 = box(0, height - slab, a, height)
+structure2 = box(0, -height + slab, a, -height)
 
 resolutions = {
     "structure": {"resolution": 0.1, "distance": 0.1},
@@ -66,7 +66,7 @@ mesh = from_meshio(
             structure=structure,
             structure1=structure1,
             structure2=structure2,
-            box=box,
+            background=background,
         ),
         resolutions=resolutions,
         filename="mesh.msh",
@@ -80,7 +80,7 @@ basis_vec = Basis(mesh, ElementTriP1() * ElementTriP1())
 basis_epsilon_r = basis_vec.with_element(ElementDG(ElementTriP1()))
 
 epsilon_r = basis_epsilon_r.zeros(dtype=np.complex64) + 1.45
-epsilon_r[basis_epsilon_r.get_dofs(elements="box")] = 1.39
+epsilon_r[basis_epsilon_r.get_dofs(elements="background")] = 1.39
 epsilon_r **= 2
 basis_epsilon_r.plot(np.real(epsilon_r), aspect=a, colorbar=True).show()
 
