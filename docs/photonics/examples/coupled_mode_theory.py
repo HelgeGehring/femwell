327f62c8386a9c52268666baa1f0fd364989038f
Marc de Cea <48526366+mdecea@users.noreply.github.com>
2023-02-28 11:39:26 -0800

    Update coupled_mode_theory.py

--- docs/photonics/examples/coupled_mode_theory.py
+++ docs/photonics/examples/coupled_mode_theory.py
@@ -129,8 +129,7 @@ plt.show()
 print("Refractive index of symmetric and assymetric mode:", lams_both)
 # https://www.fiberoptics4sale.com/blogs/wave-optics/directional-couplers
 print(
-    "Maximum power transfer after %.2f um prop length:" %
-    ( np.pi / (2 * np.pi / wavelength * np.real(lams_both[0] - lams_both[1])) ),
+    f"Maximum power transfer after {np.pi / (2 * np.pi / wavelength * np.real(lams_both[0] - lams_both[1]))} um prop length"
 )
 
 # %% [markdown]
