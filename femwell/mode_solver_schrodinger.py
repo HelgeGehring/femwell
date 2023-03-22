d68ea42ef557d66b65a545c0947d2ae055dc3364
simbilod <sb30@princeton.edu>
2023-03-21 21:50:16 -0700

    pre-commit

--- femwell/mode_solver_schrodinger.py
+++ femwell/mode_solver_schrodinger.py
@@ -8,9 +8,10 @@ from skfem import (
     condense,
     solve,
 )
-from skfem.utils import solver_eigen_scipy
-from femwell.solver import solver_eigen_slepc
 from skfem.helpers import dot, grad, inner
+from skfem.utils import solver_eigen_scipy
+
+from femwell.solver import solver_eigen_slepc
 
 mesh = MeshLine(np.linspace(-1, 1, 201))
 mesh = mesh.with_subdomains({"well": lambda p: abs(p[0]) < 0.2})
@@ -19,16 +20,18 @@ basis = Basis(mesh, ElementLineP1())
 basis_potential = basis.with_element(ElementLineP0())
 
 # Potential (eV)
-potential = basis_potential.zeros() + 10000 # units seem off?
+potential = basis_potential.zeros() + 1000  # units seem off?
 potential[basis_potential.get_dofs(elements="well")] = 0
 basis_potential.plot(potential).show()
 
 
-K0 = 7.62036790878 # hbar^2/m0 in eV, and with distances in A
+K0 = 7.62036790878  # hbar^2/m0 in eV, and with distances in A
+
 
 @BilinearForm
 def lhs(u, v, w):
     return K0 * dot(grad(u), grad(v)) + w["potential"] * inner(u, v)
+
 
 @BilinearForm
 def rhs(u, v, w):
@@ -39,7 +42,7 @@ A = lhs.assemble(basis, potential=basis_potential.interpolate(potential))
 B = rhs.assemble(basis)
 
 lams, xs = solve(
-    *condense(A, B, D=basis.get_dofs()), solver=solver_eigen_scipy(k=2, sigma=0, which='LM')
+    *condense(A, B, D=basis.get_dofs()), solver=solver_eigen_scipy(k=2, sigma=0, which="LM")
 )
 
 print(np.sqrt(lams))
