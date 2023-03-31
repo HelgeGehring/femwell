8cf07e866c3599916abc5659b194593758e14dd5
HelgeGehring <42973196+HelgeGehring@users.noreply.github.com>
2023-03-31 14:39:42 -0700

    add benchmark #40

---
+++ docs/benchmarks/mode_solver.py
@@ -0,0 +1,95 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: py:light,md:myst
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.14.4
+#   kernelspec:
+#     display_name: Python 3
+#     name: python3
+# ---
+
+# # Benchmark of the mode solver
+
+# Reproducing {cite}`Hadley2002`
+
+# + tags=["remove-stderr", "hide-input"]
+from collections import OrderedDict
+
+import numpy as np
+import pandas as pd
+import shapely
+import shapely.affinity
+from skfem import Basis, ElementTriP0
+from skfem.io.meshio import from_meshio
+
+from femwell.mesh import mesh_from_OrderedDict
+from femwell.mode_solver import compute_modes
+
+core_width = 3
+core_thickness = 1
+slab_width = 18
+box_thickness = 10
+clad_thickness = 3
+
+slab_thicknesses = [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7]
+neff_values_paper = [3.412022, 3.412126, 3.412279, 4.312492, 3.412774, 3.413132, 3.413571, 3.414100]
+neff_values_femwell = []
+
+for slab_thickness in slab_thicknesses:
+    if slab_thickness == 0.0:
+        core = shapely.Polygon(
+            (
+                (-core_width / 2, 0),
+                (-core_width / 2, core_thickness),
+                (core_width / 2, core_thickness),
+                (core_width / 2, 0),
+            )
+        )
+    else:
+        core = shapely.Polygon(
+            (
+                (-slab_width / 2, 0),
+                (-slab_width / 2, slab_thickness),
+                (-core_width / 2, slab_thickness),
+                (-core_width / 2, core_thickness),
+                (core_width / 2, core_thickness),
+                (core_width / 2, slab_thickness),
+                (slab_width / 2, slab_thickness),
+                (slab_width / 2, 0),
+            )
+        )
+
+    polygons = OrderedDict(
+        core=core,
+        box=shapely.box(-slab_width / 2, -box_thickness, slab_width / 2, 0),
+        clad=shapely.box(-slab_width / 2, clad_thickness, slab_width / 2, 0),
+    )
+
+    resolutions = dict(core={"resolution": 0.1, "distance": 3})
+
+    mesh = from_meshio(
+        mesh_from_OrderedDict(
+            polygons, resolutions, default_resolution_max=0.3, filename="mesh.msh"
+        )
+    )
+
+    basis0 = Basis(mesh, ElementTriP0())
+    epsilon = basis0.zeros()
+    for subdomain, n in {"core": 3.44, "box": 3.40, "clad": 1}.items():
+        epsilon[basis0.get_dofs(elements=subdomain)] = n**2
+
+    lams, basis, xs = compute_modes(basis0, epsilon, wavelength=1.15, num_modes=1, order=2)
+
+    neff_values_femwell.append(np.real(lams[0]))
+
+pd.DataFrame(
+    {
+        "slab_thickness": slab_thicknesses,
+        "reference value": (f"{n:.6f}" for n in neff_values_paper),
+        "calculated value": (f"{n:.7f}" for n in neff_values_femwell),
+    }
+)
