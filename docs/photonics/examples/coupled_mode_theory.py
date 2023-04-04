499e5742b8601261bd1f77c984f2b78d7c6429be
Helge Gehring <42973196+HelgeGehring@users.noreply.github.com>
2023-04-03 17:20:56 -0700

    adjust coupled_mode_theory example to object-oriented mode solver

--- docs/photonics/examples/coupled_mode_theory.py
+++ docs/photonics/examples/coupled_mode_theory.py
@@ -28,6 +28,7 @@
 
 # %% tags=["hide-input"]
 from collections import OrderedDict
+from itertools import chain
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -38,13 +39,7 @@ from skfem import Basis, ElementTriP0, Mesh
 from skfem.io import from_meshio
 
 from femwell.mesh import mesh_from_OrderedDict
-from femwell.mode_solver import (
-    calculate_coupling_coefficient,
-    calculate_hfield,
-    calculate_overlap,
-    compute_modes,
-    plot_mode,
-)
+from femwell.mode_solver import compute_modes
 
 # %% [markdown]
 # Let's set up the geometry!
@@ -119,17 +114,20 @@ basis0 = Basis(mesh, ElementTriP0(), intorder=4)
 epsilon = basis0.zeros() + 1.444**2
 epsilon[basis0.get_dofs(elements=("core_1", "core_2"))] = 3.4777**2
 # basis0.plot(epsilon, colorbar=True).show()
-lams_both, basis, xs_both = compute_modes(
-    basis0, epsilon, wavelength=wavelength, mu_r=1, num_modes=2
+modes_both = compute_modes(
+    basis0, epsilon, wavelength=wavelength, mu_r=1, num_modes=2, return_objects=True
 )
-plot_mode(basis, np.real(xs_both[0]), direction="x")
-plt.show()
-plot_mode(basis, np.real(xs_both[1]), direction="x")
-plt.show()
-print("Refractive index of symmetric and assymetric mode:", lams_both)
+modes_both[0].show(modes_both[0].E.real, direction="x")
+modes_both[1].show(modes_both[1].E.real, direction="x")
+print(
+    "Refractive index of symmetric and assymetric mode:",
+    modes_both[0].n_eff,
+    ", ",
+    modes_both[1].n_eff,
+)
 # https://www.fiberoptics4sale.com/blogs/wave-optics/directional-couplers
 print(
-    f"Maximum power transfer after {np.pi / (2 * np.pi / wavelength * np.real(lams_both[0] - lams_both[1]))} um prop length"
+    f"Maximum power transfer after {np.pi / (2 * np.pi / wavelength * np.real(modes_both[0].n_eff - modes_both[1].n_eff))} um prop length"
 )
 
 # %% [markdown]
@@ -139,18 +137,20 @@ print(
 epsilon = basis0.zeros() + 1.444**2
 epsilon[basis0.get_dofs(elements="core_1")] = 3.4777**2
 # basis0.plot(epsilon, colorbar=True).show()
-lams_1, basis, xs_1 = compute_modes(basis0, epsilon, wavelength=wavelength, mu_r=1, num_modes=1)
-print("Effective refractive index of the mode of the first waveguide", lams_1)
-plot_mode(basis, np.real(xs_1[0]), direction="x")
-plt.show()
+modes_1 = compute_modes(
+    basis0, epsilon, wavelength=wavelength, mu_r=1, num_modes=1, return_objects=True
+)
+print("Effective refractive index of the mode of the first waveguide", modes_1[0].n_eff)
+modes_1[0].show(modes_1[0].E.real, direction="x")
 
 epsilon_2 = basis0.zeros() + 1.444**2
 epsilon_2[basis0.get_dofs(elements="core_2")] = 3.4777**2
 # basis0.plot(epsilon_2, colorbar=True).show()
-lams_2, basis, xs_2 = compute_modes(basis0, epsilon_2, wavelength=wavelength, mu_r=1, num_modes=1)
-print("Effective refractive index of the mode of the second waveguide", lams_2)
-plot_mode(basis, np.real(xs_2[0]), direction="x")
-plt.show()
+modes_2 = compute_modes(
+    basis0, epsilon_2, wavelength=wavelength, mu_r=1, num_modes=1, return_objects=True
+)
+print("Effective refractive index of the mode of the second waveguide", modes_2[0].n_eff)
+modes_2[0].show(modes_2[0].E.real, direction="x")
 
 # %%
 length = 200
@@ -158,41 +158,27 @@ ts = np.linspace(0, length, 1000)
 
 # %%
 epsilons = [epsilon, epsilon_2]
-modes = [(lam, x, 0) for lam, x in zip(lams_1, xs_1)] + [
-    (lam, x, 1) for lam, x in zip(lams_2, xs_2)
-]
 
-overlap_integrals = np.zeros((len(modes), len(modes)), dtype=complex)
-for i, (lam_i, E_i, epsilon_i) in enumerate(modes):
-    for j, (lam_j, E_j, epsilon_j) in enumerate(modes):
-        H_i = calculate_hfield(
-            basis,
-            E_i,
-            lam_i * (2 * np.pi / 1.55),
-            omega=2 * np.pi / wavelength * speed_of_light,
-        )
-        H_j = calculate_hfield(
-            basis,
-            E_j,
-            lam_j * (2 * np.pi / 1.55),
-            omega=2 * np.pi / wavelength * speed_of_light,
-        )
-        overlap_integrals[i, j] = calculate_overlap(basis, E_i, H_i, basis, E_j, H_j)
+num_modes = len(modes_1) + len(modes_2)
+overlap_integrals = np.zeros((num_modes, num_modes), dtype=complex)
+for i, mode_i in enumerate(chain(modes_1, modes_2)):
+    for j, mode_j in enumerate(chain(modes_1, modes_2)):
+        overlap_integrals[i, j] = mode_i.calculate_overlap(mode_j)
 
 print("overlap", overlap_integrals)
 # plt.imshow(np.abs(overlap_integrals))
 # plt.colorbar()
 # plt.show()
 
-coupling_coefficients = np.zeros((len(modes), len(modes)), dtype=complex)
-for i, (lam_i, E_i, epsilon_i) in enumerate(modes):
-    for j, (lam_j, E_j, epsilon_j) in enumerate(modes):
+coupling_coefficients = np.zeros((num_modes, num_modes), dtype=complex)
+for i, mode_i in enumerate(chain(modes_1, modes_2)):
+    for j, mode_j in enumerate(chain(modes_1, modes_2)):
         coupling_coefficients[i, j] = (
             k0
             * speed_of_light
             * epsilon_0
-            * calculate_coupling_coefficient(
-                basis0, epsilons[(epsilon_j + 1) % 2] - 1.444**2, basis, E_i, E_j
+            * mode_i.calculate_coupling_coefficient(
+                mode_j, epsilons[(j // len(modes_1) + 1) % 2] - 1.444**2
             )
             * 0.5
         )
@@ -225,8 +211,10 @@ kappas = np.array(
 )
 print(kappas)
 
-delta = 0.5 * (np.real(lams_1[0]) * k0 + kappas[1, 1] - (np.real(lams_2[0]) * k0 + kappas[0, 0]))
-print(delta, np.real(lams_1[0]) * k0, kappas[1, 1])
+delta = 0.5 * (
+    np.real(modes_1[0].n_eff) * k0 + kappas[1, 1] - (np.real(modes_2[0].n_eff) * k0 + kappas[0, 0])
+)
+print(delta, np.real(modes_1[0].n_eff) * k0, kappas[1, 1])
 
 beta_c = (kappas[0, 1] * kappas[1, 0] + delta**2) ** 0.5
 
@@ -246,8 +234,11 @@ plt.show()
 # %%
 def fun(t, y):
     phase_matrix = [
-        [np.exp(2j * np.pi / wavelength * (lam_i - lam_j) * t) for lam_j, E_j, epsilon_j in modes]
-        for lam_i, E_i, epsilon_i in modes
+        [
+            np.exp(2j * np.pi / wavelength * (mode_i.n_eff - mode_j.n_eff) * t)
+            for mode_j in chain(modes_1, modes_2)
+        ]
+        for mode_i in chain(modes_1, modes_2)
     ]
     matrix = (
         np.linalg.inv(overlap_integrals * phase_matrix)
@@ -271,28 +262,19 @@ plt.show()
 # %%
 R = []
 
-lam_i = lams_1[0]
-E_i = xs_1[0]
+lam_i = modes_1[0].n_eff
+E_i = modes_1[0].E
 
-for lam_j, E_j in zip(lams_both, xs_both):
-    H_i = calculate_hfield(
-        basis,
-        E_i,
-        lam_i * (2 * np.pi / 1.55),
-        omega=2 * np.pi / wavelength * speed_of_light,
-    )
-    H_j = calculate_hfield(
-        basis,
-        E_j,
-        lam_j * (2 * np.pi / 1.55),
-        omega=2 * np.pi / wavelength * speed_of_light,
-    )
-    R.append(np.abs(calculate_overlap(basis, E_i, H_i, basis, E_j, H_j) ** 2))
+for mode_j in modes_both:
+    R.append(np.abs(mode_i.calculate_overlap(mode_j) ** 2))
 print(R)
 P = (
     R[0] ** 2
     + R[1] ** 2
-    + 2 * R[0] * R[1] * np.cos(2 * np.pi / wavelength * (lams_both[0] - lams_both[1]) * ts)
+    + 2
+    * R[0]
+    * R[1]
+    * np.cos(2 * np.pi / wavelength * (modes_both[0].n_eff - modes_both[1].n_eff) * ts)
 )
 
 plt.plot(ts, P)
