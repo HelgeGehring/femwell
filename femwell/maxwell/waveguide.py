28aa4aefaad84946b86f8e4c3ebf2af5d6aeb47c
Helge Gehring <42973196+HelgeGehring@users.noreply.github.com>
2023-04-09 21:54:48 -0700

    remove non-object oriented mode solver

--- femwell/maxwell/waveguide.py
+++ femwell/maxwell/waveguide.py
@@ -28,16 +28,6 @@ from skfem import (
 from skfem.helpers import cross, curl, dot, grad, inner
 from skfem.utils import solver_eigen_scipy
 
-from femwell.mode_solver import (
-    calculate_coupling_coefficient,
-    calculate_energy_current_density,
-    calculate_hfield,
-    calculate_overlap,
-    calculate_scalar_product,
-    confinement_factor,
-    plot_mode,
-)
-
 
 @dataclass(frozen=True)
 class Mode:
@@ -282,6 +272,198 @@ def compute_modes(
             for i in range(num_modes)
         ]
     )
+
+
+def calculate_hfield(basis, xs, beta, omega=1):
+    @BilinearForm(dtype=np.complex64)
+    def aform(e_t, e_z, v_t, v_z, w):
+        return (
+            (-1j * beta * e_t[1] + e_z.grad[1]) * v_t[0]
+            + (1j * beta * e_t[0] - e_z.grad[0]) * v_t[1]
+            + e_t.curl * v_z
+        )
+
+    @BilinearForm(dtype=np.complex64)
+    def bform(e_t, e_z, v_t, v_z, w):
+        return dot(e_t, v_t) + e_z * v_z
+
+    return (
+        scipy.sparse.linalg.spsolve(
+            bform.assemble(basis), aform.assemble(basis) @ xs.astype(complex)
+        )
+        * -1j
+        / scipy.constants.mu_0
+        / omega
+    )
+
+
+def calculate_energy_current_density(basis, xs):
+    basis_energy = basis.with_element(ElementTriP0())
+
+    @LinearForm(dtype=complex)
+    def aform(v, w):
+        e_t, e_z = w["e"]
+        return abs(e_t[0]) ** 2 * v + abs(e_t[1]) ** 2 * v + abs(e_z) * v
+
+    a_operator = aform.assemble(basis_energy, e=basis.interpolate(xs))
+
+    @BilinearForm(dtype=complex)
+    def bform(e, v, w):
+        return e * v
+
+    b_operator = bform.assemble(basis_energy)
+
+    return basis_energy, scipy.sparse.linalg.spsolve(b_operator, a_operator)
+
+
+def calculate_overlap(basis_i, E_i, H_i, basis_j, E_j, H_j):
+    @Functional(dtype=np.complex64)
+    def overlap(w):
+        return cross(np.conj(w["E_i"][0]), w["H_j"][0]) + cross(w["E_j"][0], np.conj(w["H_i"][0]))
+
+    if basis_i == basis_j:
+        return 0.5 * overlap.assemble(
+            basis_i,
+            E_i=basis_i.interpolate(E_i),
+            H_i=basis_i.interpolate(H_i),
+            E_j=basis_j.interpolate(E_j),
+            H_j=basis_j.interpolate(H_j),
+        )
+    basis_j_fix = basis_j.with_element(ElementVector(ElementTriP1()))
+
+    (et, et_basis), (ez, ez_basis) = basis_j.split(E_j)
+    E_j = basis_j_fix.project(et_basis.interpolate(et), dtype=np.cfloat)
+    (et_x, et_x_basis), (et_y, et_y_basis) = basis_j_fix.split(E_j)
+
+    (et, et_basis), (ez, ez_basis) = basis_j.split(H_j)
+    H_j = basis_j_fix.project(et_basis.interpolate(et), dtype=np.cfloat)
+    (ht_x, ht_x_basis), (ht_y, ht_y_basis) = basis_j_fix.split(H_j)
+
+    @Functional(dtype=np.complex64)
+    def overlap(w):
+        return cross(
+            np.conj(w["E_i"][0]),
+            np.array((ht_x_basis.interpolator(ht_x)(w.x), ht_y_basis.interpolator(ht_y)(w.x))),
+        ) + cross(
+            np.array((et_x_basis.interpolator(et_x)(w.x), et_y_basis.interpolator(et_y)(w.x))),
+            np.conj(w["H_i"][0]),
+        )
+
+    return 0.5 * overlap.assemble(
+        basis_i, E_i=basis_i.interpolate(E_i), H_i=basis_i.interpolate(H_i)
+    )
+
+
+def calculate_scalar_product(basis_i, E_i, basis_j, H_j):
+    @Functional
+    def overlap(w):
+        return cross(np.conj(w["E_i"][0]), w["H_j"][0])
+
+    if basis_i == basis_j:
+        return overlap.assemble(basis_i, E_i=basis_i.interpolate(E_i), H_j=basis_j.interpolate(H_j))
+    basis_j_fix = basis_j.with_element(ElementVector(ElementTriP1()))
+
+    (et, et_basis), (ez, ez_basis) = basis_j.split(H_j)
+    H_j = basis_j_fix.project(et_basis.interpolate(et), dtype=np.cfloat)
+    (ht_x, ht_x_basis), (ht_y, ht_y_basis) = basis_j_fix.split(H_j)
+
+    @Functional(dtype=np.complex64)
+    def overlap(w):
+        return cross(
+            np.conj(w["E_i"][0]),
+            np.array((ht_x_basis.interpolator(ht_x)(w.x), ht_y_basis.interpolator(ht_y)(w.x))),
+        )
+
+    return overlap.assemble(basis_i, E_i=basis_i.interpolate(E_i))
+
+
+def calculate_coupling_coefficient(basis_epsilon, delta_epsilon, basis, E_i, E_j):
+    @Functional(dtype=complex)
+    def overlap(w):
+        return w["delta_epsilon"] * (
+            dot(np.conj(w["E_i"][0]), w["E_j"][0]) + np.conj(w["E_i"][1]) * w["E_j"][1]
+        )
+
+    return overlap.assemble(
+        basis,
+        E_i=basis.interpolate(E_i),
+        E_j=basis.interpolate(E_j),
+        delta_epsilon=basis_epsilon.interpolate(delta_epsilon),
+    )
+
+
+def confinement_factor(basis_epsilon, epsilon, basis, E):
+    @Functional
+    def factor(w):
+        return (
+            (
+                np.sqrt(w["epsilon"])
+                * (dot(np.conj(w["E"][0]), w["E"][0]) + np.conj(w["E"][1]) * w["E"][1])
+            )
+            * speed_of_light
+            * epsilon_0
+        )
+
+    return factor.assemble(
+        basis,
+        E=basis.interpolate(E),
+        epsilon=basis_epsilon.interpolate(epsilon),
+    )
+
+
+def plot_mode(basis, mode, plot_vectors=False, colorbar=True, title="E", direction="y"):
+    from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+    (et, et_basis), (ez, ez_basis) = basis.split(mode)
+
+    if plot_vectors:
+        rc = (2, 1) if direction != "x" else (1, 2)
+        fig, axs = plt.subplots(*rc, subplot_kw=dict(aspect=1))
+        for ax in axs:
+            basis.mesh.draw(ax=ax, boundaries=True, boundaries_only=True)
+            for subdomain in basis.mesh.subdomains.keys() - {"gmsh:bounding_entities"}:
+                basis.mesh.restrict(subdomain).draw(ax=ax, boundaries_only=True)
+        et_basis.plot(et, ax=axs[0])
+        ez_basis.plot(ez, ax=axs[1])
+
+        divider = make_axes_locatable(axs[1])
+        cax = divider.append_axes("right", size="5%", pad=0.05)
+        plt.colorbar(axs[1].collections[0], cax=cax)
+        return fig, axs
+
+    plot_basis = et_basis.with_element(ElementVector(ElementDG(ElementTriP1())))
+    et_xy = plot_basis.project(et_basis.interpolate(et))
+    (et_x, et_x_basis), (et_y, et_y_basis) = plot_basis.split(et_xy)
+
+    rc = (3, 1) if direction != "x" else (1, 3)
+    fig, axs = plt.subplots(*rc, subplot_kw=dict(aspect=1))
+    for ax in axs:
+        basis.mesh.draw(ax=ax, boundaries=True, boundaries_only=True)
+        for subdomain in basis.mesh.subdomains.keys() - {"gmsh:bounding_entities"}:
+            basis.mesh.restrict(subdomain).draw(ax=ax, boundaries_only=True)
+
+    for ax, component in zip(axs, "xyz"):
+        ax.set_title(f"${title}_{component}$")
+
+    maxabs = max(np.max(np.abs(data.value)) for data in basis.interpolate(mode))
+    vmin = -maxabs if colorbar == "same" else None
+    vmax = maxabs if colorbar == "same" else None
+
+    et_x_basis.plot(et_x, shading="gouraud", ax=axs[0], vmin=vmin, vmax=vmax)
+    et_y_basis.plot(et_y, shading="gouraud", ax=axs[1], vmin=vmin, vmax=vmax)
+    ez_basis.plot(ez, shading="gouraud", ax=axs[2], vmin=vmin, vmax=vmax)
+
+    if colorbar:
+        if colorbar == "same":
+            plt.colorbar(axs[0].collections[-1], ax=axs.ravel().tolist())
+        else:
+            for ax in axs:
+                divider = make_axes_locatable(ax)
+                cax = divider.append_axes("right", size="5%", pad=0.05)
+                plt.colorbar(ax.collections[-1], cax=cax)
+            plt.tight_layout()
+
+    return fig, axs
 
 
 if __name__ == "__main__":
