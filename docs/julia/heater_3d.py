ca64e6aa5fe0c1ee498f3ff6953daab116857836
Helge Gehring <42973196+HelgeGehring@users.noreply.github.com>
2023-10-18 12:58:46 -0700

    fix heater_3d

--- docs/julia/heater_3d.py
+++ docs/julia/heater_3d.py
@@ -1,14 +1,17 @@
 import gdsfactory as gf
 from gdsfactory.generic_tech import LAYER
 from gdsfactory.pdk import LayerStack, get_layer_stack
-from gplugins.gmsh.xyz_mesh import xyz_mesh
+from gplugins.common.utils.get_component_with_net_layers import (
+    get_component_with_net_layers,
+)
+from gplugins.gmsh.get_mesh import get_mesh
 
 # Choose some component
 c = gf.component.Component()
 waveguide = c << gf.get_component(gf.components.straight_heater_metal(length=40))
-e1 = c << gf.components.straight(1, cross_section="metal3")
+e1 = c << gf.components.straight(1, cross_section="xs_m3")
 e1.connect(e1["e1"], waveguide["l_e1"])
-e2 = c << gf.components.straight(1, cross_section="metal3")
+e2 = c << gf.components.straight(1, cross_section="xs_m3")
 e2.connect(e2["e1"], waveguide["r_e3"])
 c.add_port("e1", port=e1["e2"])
 c.add_port("e2", port=e2["e2"])
@@ -19,9 +22,10 @@ wafer = c << gf.components.bbox(bbox=waveguide.bbox, layer=LAYER.WAFER)
 
 # Generate a new component and layerstack with new logical layers
 layerstack = get_layer_stack()
-c = layerstack.get_component_with_net_layers(
+c = get_component_with_net_layers(
     c,
-    portnames=["e1", "e2"],
+    layerstack,
+    port_names=["e1", "e2"],
     delimiter="#",
 )
 
@@ -57,17 +61,18 @@ filtered_layerstack.layers["clad"].thickness += sum(
 
 resolutions = {
     "core": {"resolution": 0.3},
-    "via2": {"resolution": 0.1},
-    "via1": {"resolution": 0.1},
-    "heater": {"resolution": 0.1},
+    "via2": {"resolution": 0.2},
+    "via1": {"resolution": 0.2},
+    "heater": {"resolution": 0.2},
 }
-geometry = xyz_mesh(
+geometry = get_mesh(
+    type="3D",
     component=c,
-    layerstack=filtered_layerstack,
+    layer_stack=filtered_layerstack,
     resolutions=resolutions,
     filename="mesh.msh",
-    default_characteristic_length=0.5,
-    global_3D_algorithm=10,
+    default_characteristic_length=2,
+    # global_3D_algorithm=10,
     verbosity=5,
     global_scaling=1e-6,
 )
