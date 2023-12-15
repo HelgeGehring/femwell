1045b9b7d37e733ce11603bb9dd63f7294e22bf5
Helge Gehring <42973196+HelgeGehring@users.noreply.github.com>
2023-12-14 22:21:15 -0800

    convert to use percent

--- docs/photonics/examples/waveguide_modes.py
+++ docs/photonics/examples/waveguide_modes.py
@@ -1,20 +1,21 @@
 # ---
 # jupyter:
 #   jupytext:
-#     formats: py:light,md:myst
+#     formats: py:percent,md:myst
 #     text_representation:
 #       extension: .py
-#       format_name: light
-#       format_version: '1.5'
-#       jupytext_version: 1.14.4
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.15.0
 #   kernelspec:
 #     display_name: Python 3
 #     name: python3
 # ---
 
+# %% [markdown]
 # # Modes of a rectangular waveguide
 
-# + tags=["remove-stderr", "hide-input", "thebe-init"]
+# %% tags=["remove-stderr", "hide-input", "thebe-init"]
 from collections import OrderedDict
 
 import matplotlib.pyplot as plt
@@ -30,15 +31,14 @@ from femwell.maxwell.waveguide import compute_modes
 from femwell.mesh import mesh_from_OrderedDict
 from femwell.visualization import plot_domains
 
-# -
-
+# %% [markdown]
 # We describe the geometry using shapely.
 # In this case it's simple: we use a shapely.box for the waveguide.
 # For the surrounding we buffer the core and clip it to the part below the waveguide for the box.
 # The remaining buffer is used as the clad.
 # For the core we set the resolution to 30nm and let it fall of over 500nm
 
-# +
+# %%
 wg_width = 2.5
 wg_thickness = 0.3
 core = shapely.geometry.box(-wg_width / 2, 0, +wg_width / 2, wg_thickness)
@@ -54,30 +54,30 @@ resolutions = dict(core={"resolution": 0.03, "distance": 0.5})
 
 mesh = from_meshio(mesh_from_OrderedDict(polygons, resolutions, default_resolution_max=10))
 mesh.draw().show()
-# -
 
+# %% [markdown]
 # Let's also plot the domains
 
-# +
+# %%
 plot_domains(mesh)
 plt.show()
-# -
 
+# %% [markdown]
 # On this mesh, we define the epsilon. We do this by setting domainwise the epsilon to the squared refractive index.
 
-# +
+# %%
 basis0 = Basis(mesh, ElementTriP0())
 epsilon = basis0.zeros()
 for subdomain, n in {"core": 1.9963, "box": 1.444, "clad": 1}.items():
     epsilon[basis0.get_dofs(elements=subdomain)] = n**2
 basis0.plot(epsilon, colorbar=True).show()
-# -
 
+# %% [markdown]
 # And now we call `compute_modes` to calculate the modes of the waveguide we set up.
 # As modes can have complex fields as soon as the epsilon gets complex, so we get a complex field for each mode.
 # Here we show only the real part of the mode.
 
-# +
+# %%
 wavelength = 1.55
 
 modes = compute_modes(basis0, epsilon, wavelength=wavelength, num_modes=2, order=2)
@@ -86,23 +86,26 @@ for mode in modes:
     mode.show(mode.E.real, colorbar=True, direction="x")
     mode.show(mode.E.imag, colorbar=True, direction="x")
 
-# -
 
+# %% [markdown]
 # The intensity can be plotted directly from the mode object
 # +
 
+# %%
 fig, ax = plt.subplots()
 modes[0].plot_intensity(ax=ax)
 plt.title("Normalized Intensity")
 plt.tight_layout()
 plt.show()
 
+# %% [markdown]
 # -
 
+# %% [markdown]
 # Now, let's calculate with the modes:
 # What percentage of the mode is within the core for the calculated modes?
 
-# +
+# %%
 powers_in_waveguide = []
 confinement_factors_waveguide = []
 
