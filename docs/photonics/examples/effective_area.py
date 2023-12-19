0395f4cc67cff2888d32aecc6920a99326f81e8d
Helge Gehring <42973196+HelgeGehring@users.noreply.github.com>
2023-12-18 21:12:47 -0800

    adjust definition of transversality

--- docs/photonics/examples/effective_area.py
+++ docs/photonics/examples/effective_area.py
@@ -39,13 +39,13 @@ from femwell.mesh import mesh_from_OrderedDict
 wavelength = 1.55
 capital_w = 1.4
 capital_h = 0.3
-h_list = [0.5,0.7]
+h_list = [0.5, 0.7]
 t = 0.1
 n_silicon = 3.48
 n_air = 1
 n_nc = 1.6
 n_silica = 1.45
-w_list = [x for x in range(50, 700, 10)]  # x*1e-3 for x in range(50,700,10)
+w_list = [x for x in range(250, 700, 100)]
 neff_dict = dict()
 aeff_dict = dict()
 tm_dict = dict()
@@ -65,16 +65,16 @@ for h in h_list:
             core=nc,
             silicon=silicon,
             silica=silica,
-            air=nc.buffer(5.0, resolution=5),
+            air=nc.buffer(10.0, resolution=4),
         )
 
         resolutions = dict(
-            core={"resolution": 0.03, "distance": 0.1},
-            silicon={"resolution": 0.03, "distance": 0.1},
-            silica={"resolution": 0.03, "distance": 0.1},
+            core={"resolution": 0.02, "distance": 0.3},
+            silicon={"resolution": 0.02, "distance": 0.1},
+            silica={"resolution": 0.04, "distance": 0.5},
         )
 
-        mesh = from_meshio(mesh_from_OrderedDict(polygons, resolutions, default_resolution_max=1))
+        mesh = from_meshio(mesh_from_OrderedDict(polygons, resolutions, default_resolution_max=2))
 
         basis0 = Basis(mesh, ElementTriP0())
         epsilon = basis0.zeros()
@@ -84,22 +84,22 @@ for h in h_list:
             "air": n_air,
             "silica": n_silica,
         }.items():
-            epsilon[basis0.get_dofs(elements=subdomain)] = n ** 2
-        modes = compute_modes(basis0, epsilon, wavelength=wavelength, num_modes=4, order=1)
+            epsilon[basis0.get_dofs(elements=subdomain)] = n**2
+        modes = compute_modes(basis0, epsilon, wavelength=wavelength, num_modes=3, order=1)
 
         for mode in modes:
             if mode.tm_fraction > 0.5:
                 # mode.show(np.real(mode.E))
                 print(f"Effective refractive index: {mode.n_eff:.4f}")
                 print(f"Effective mode area: {mode.calculate_effective_area(field='y'):.4f}")
-                print(f"tm fraction: {mode.tm_fraction}")
+                print(f"Mode transversality: {mode.transversality}")
                 neff_list.append(np.real(mode.n_eff))
                 aeff_list.append(mode.calculate_effective_area())
-                tm_list.append(mode.tm_fraction)
+                tm_list.append(mode.transversality)
                 break
         else:
             print(f"no TM mode found for {width}")
-    neff_dict[str(h)]=neff_list
+    neff_dict[str(h)] = neff_list
     aeff_dict[str(h)] = aeff_list
     tm_dict[str(h)] = tm_list
 # %% [markdown]
@@ -134,7 +134,7 @@ ref_tm_700nm_x, ref_tm_700nm_y = np.split(reference_tm_700nm.values, 2, axis=1)
 
 fig, (ax1, ax2, ax3, ax4) = plt.subplots(4, 1, figsize=(9, 20))
 
-ax1.plot(w_list, aeff_dict['0.5'], "-o", c="r", label="stimulated aeff")
+ax1.plot(w_list, aeff_dict["0.5"], "-o", c="r", label="stimulated aeff")
 ax1.scatter(ref_aeff_500nm_x, ref_aeff_500nm_y, c="b", label="reference aeff")
 ax1.set_title("aeff at h = 500nm")
 ax1.yaxis.set_major_locator(MultipleLocator(0.05))
@@ -144,12 +144,12 @@ ax1.legend()
 
 ax2b = ax2.twinx()
 ax2b.set_ylabel("mode transversality")
-ax2b.scatter(ref_tm_500nm_x, ref_tm_500nm_y,marker="v", c="b", label="reference transversality")
-ax2b.plot(w_list, tm_dict['0.5'], "-v", c="r", label="stimulated transversality")
-ax2b.set_ylim(0.775,1)
+ax2b.scatter(ref_tm_500nm_x, ref_tm_500nm_y, marker="v", c="b", label="reference transversality")
+ax2b.plot(w_list, tm_dict["0.5"], "-v", c="r", label="stimulated transversality")
+ax2b.set_ylim(0.775, 1)
 ax2b.legend()
 
-ax2.plot(w_list, neff_dict['0.5'], "-o", c="r", label="stimulated neff")
+ax2.plot(w_list, neff_dict["0.5"], "-o", c="r", label="stimulated neff")
 ax2.scatter(ref_neff_500nm_x, ref_neff_500nm_y, c="b", label="reference neff")
 ax2.set_title("neff and mode transversality at h = 500nm")
 ax2.set_ylabel("neff")
@@ -158,7 +158,7 @@ ax2.yaxis.set_minor_locator(MultipleLocator(0.2))
 ax2.set_ylim(0, 2.8)
 ax2.legend()
 
-ax3.plot(w_list, aeff_dict['0.7'], "-o", c="r", label="stimulated aeff")
+ax3.plot(w_list, aeff_dict["0.7"], "-o", c="r", label="stimulated aeff")
 ax3.scatter(ref_aeff_700nm_x, ref_aeff_700nm_y, c="b", label="reference aeff")
 ax3.set_title("aeff at h = 700nm")
 ax3.yaxis.set_major_locator(MultipleLocator(0.05))
@@ -168,13 +168,13 @@ ax3.legend()
 
 ax4b = ax4.twinx()
 ax4b.set_ylabel("mode transversality")
-ax4b.scatter(ref_tm_700nm_x, ref_tm_700nm_y,marker="v", c="b", label="reference transversality")
-ax4b.plot(w_list, tm_dict['0.7'], "-v", c="r", label="stimulated transversality")
-ax4b.set_ylim(0.775,1)
+ax4b.scatter(ref_tm_700nm_x, ref_tm_700nm_y, marker="v", c="b", label="reference transversality")
+ax4b.plot(w_list, tm_dict["0.7"], "-v", c="r", label="stimulated transversality")
+ax4b.set_ylim(0.775, 1)
 ax4b.legend()
 
 
-ax4.plot(w_list, neff_dict['0.7'], "-o", c="r", label="stimulated aeff")
+ax4.plot(w_list, neff_dict["0.7"], "-o", c="r", label="stimulated aeff")
 ax4.scatter(ref_neff_700nm_x, ref_neff_700nm_y, c="b", label="reference aeff")
 ax4.set_title("neff and mode transversality at h = 700nm")
 ax2.yaxis.set_major_locator(MultipleLocator(0.4))
