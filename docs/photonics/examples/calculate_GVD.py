e3d1c965b0ad2a9366b0ae0382971f2608ceb196
Eliza Leung <87836029+elizaleung830@users.noreply.github.com>
2024-01-19 20:53:13 +1030

    pre-commmit and fix make docs error

--- docs/photonics/examples/calculate_GVD.py
+++ docs/photonics/examples/calculate_GVD.py
@@ -11,26 +11,32 @@
 #     display_name: Python 3
 #     name: python3
 # ---
+
 # %% [markdown]
 # # Calculate GVD of waveguide
 
+
 # %% [markdown]
-# # Reproduce figure 2e from {cite}`Klenner2016`
+# Reproduce figure 2e from {cite}`Klenner2016`
+
+
+import math
+from collections import OrderedDict
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
 # %% tags=["remove-stderr", "hide-input", "thebe-init"]
 import shapely
-from collections import OrderedDict
-import math
 from scipy.interpolate import UnivariateSpline
-from femwell.visualization import plot_domains
-from femwell.mesh import mesh_from_OrderedDict
-from femwell.maxwell.waveguide import compute_modes
 from skfem import Basis, ElementTriP0
 from skfem.io import from_meshio
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-import pickle
 from tqdm import tqdm
+
+from femwell.maxwell.waveguide import compute_modes
+from femwell.mesh import mesh_from_OrderedDict
+from femwell.visualization import plot_domains
 
 # %% [markdown]
 # First, construct the geometry of waveguide according to figure 2d of the paper.
@@ -54,9 +60,11 @@ polygon = OrderedDict(
 )
 
 # Define material property and resolution of waveguide
-resolutions = dict(core={"resolution": 0.02, "distance": 0.3},
-                   cladding={"resolution": 0.05, "distance": 0.3},
-                   buried_oxide={"resolution": 0.05, "distance": 0.3})
+resolutions = dict(
+    core={"resolution": 0.02, "distance": 0.3},
+    cladding={"resolution": 0.05, "distance": 0.3},
+    buried_oxide={"resolution": 0.05, "distance": 0.3},
+)
 
 mesh = from_meshio(mesh_from_OrderedDict(polygon, resolutions, default_resolution_max=2))
 mesh.draw().show()
@@ -67,12 +75,15 @@ plt.show()
 # %% [markdown]
 # Use sellmeier equation to determine the refractive index of material.
 
+
 # %%
 def n_Si3N4(wavelength, fit=False):
     if wavelength >= 0.31 and wavelength <= 5.507:
         return math.sqrt(
-            (3.0249 * wavelength ** 2) / (wavelength ** 2 - 0.1353406 ** 2) + (40314 * wavelength ** 2) / (
-                    wavelength ** 2 - 1239.842 ** 2) + 1)
+            (3.0249 * wavelength**2) / (wavelength**2 - 0.1353406**2)
+            + (40314 * wavelength**2) / (wavelength**2 - 1239.842**2)
+            + 1
+        )
     else:
         raise ValueError(f"wavelength provided is {wavelength}um, is out of the range for Si3N4")
 
@@ -80,9 +91,12 @@ def n_Si3N4(wavelength, fit=False):
 def n_SiO2(wavelength):
     if wavelength < 0.21 or wavelength > 6.7:
         raise ValueError(f"wavelength provided is {wavelength}um, is out of the range for {type}")
-    return np.sqrt(0.6961663 * wavelength ** 2 / (wavelength ** 2 - 0.0684043 ** 2) + (
-            0.4079426 * wavelength ** 2 / (wavelength ** 2 - 0.1162414 ** 2)) + (
-                           0.8974794 * wavelength ** 2 / (wavelength ** 2 - 9.896161 ** 2)) + 1)
+    return np.sqrt(
+        0.6961663 * wavelength**2 / (wavelength**2 - 0.0684043**2)
+        + (0.4079426 * wavelength**2 / (wavelength**2 - 0.1162414**2))
+        + (0.8974794 * wavelength**2 / (wavelength**2 - 9.896161**2))
+        + 1
+    )
 
 
 n_dict = {"core": n_Si3N4, "cladding": n_SiO2, "buried_oxide": n_SiO2}
@@ -97,7 +111,6 @@ epsilon = basis0.zeros()
 wavelength_list = np.linspace(wavelength_range[0], wavelength_range[1], wavelegnth_step)
 
 for wavelength in tqdm(wavelength_list):
-
     wavelength = wavelength * 1e-3
     for subdomain, n in n_dict.items():
         epsilon[basis0.get_dofs(elements=subdomain)] = n(wavelength) ** 2
@@ -124,7 +137,7 @@ axs[0].set_xlabel("Wavelength / nm")
 axs[0].set_ylabel("neff")
 axs[0].set_title(" neff vs wavelength fit")
 axs[0].semilogy(x_range, y_spl(x_range))
-axs[0].semilogy(wavelength_list, neff_list, 'ro', label='data')
+axs[0].semilogy(wavelength_list, neff_list, "ro", label="data")
 axs[0].legend()
 axs[0].set_xlim(500, 2200)
 
@@ -140,7 +153,7 @@ ref_gvd_x, ref_gvd_y = np.split(ref_gvd.values, 2, axis=1)
 axs[2].plot(ref_gvd_x, ref_gvd_y, c="green", label="paper")
 
 # ----Calculate and plot GVD
-GVD = (-wavelength_list / (2.99792e-7) * y_spl_2d(wavelength_list))
+GVD = -wavelength_list / (2.99792e-7) * y_spl_2d(wavelength_list)
 axs[2].scatter(wavelength_list, GVD, label="calculated", c="red")
 
 axs[2].set_ylabel("GVD")
