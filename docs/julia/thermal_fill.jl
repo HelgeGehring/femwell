c87e426ac952a083464e6e1c80a9b752ff3e225a
Helge Gehring <42973196+HelgeGehring@users.noreply.github.com>
2024-02-21 17:28:28 -0800

    add text

--- docs/julia/thermal_fill.jl
+++ docs/julia/thermal_fill.jl
@@ -11,11 +11,31 @@
 #   kernelspec:
 #     display_name: base
 #     language: julia
-#     name: julia-1.9
+#     name: julia-1.10
 # ---
 
 # %% [markdown]
 # # Effective thermal conductivity
+
+# Usually, after the design of the circuitry, the empty space on chips gets filled with equally spaced small rectangles in each layer.
+# Optically, these structures are supposed to be far enough away that their influence on the structures can be neglected.
+# But for thermal considerations, those fill structures can have an impact on the temperature distribution on the chip and thus e.g. on the crosstalk between thermal phase shifters.
+# As it's computationally challenging to include all the small cuboids in the model (which is especially for the meshing a major challenge),
+# a preferable approach is to consider the filled area as a homogenous area of higher thermal conductivity.
+# For this, we calculate the effective thermal conductivity of the filled area by examining a single unit cell.
+
+# To have an intuitively understandable problem, we consider half of the unit cell to be filled with a highly thermally conductive material (metal/silicon) surrounded by a material with low thermal conductance (e.g. silicon dioxide)
+# Let's start with the mesh!
+
+# %% 
+
+unitcell_width = 2
+unitcell_length = 2
+unitcell_thickness = 2
+
+fill_width = 2
+fill_length = 1
+fill_thickness = 2
 
 # %% tags=["hide-input", "thebe-init", "remove-stderr"]
 using PyCall
@@ -28,14 +48,6 @@ Prism = pyimport("meshwell.prism").Prism
 Model = pyimport("meshwell.model").Model
 
 model = Model()
-
-unitcell_width = 2
-unitcell_length = 2
-unitcell_thickness = 2
-
-fill_width = 2
-fill_length = 1
-fill_thickness = 2
 
 fill_polygon = shapely.box(
     xmin = unitcell_length / 2 - fill_length / 2,
@@ -142,14 +154,12 @@ using GridapMakie, CairoMakie
 using Femwell.Maxwell.Electrostatic
 using Femwell.Thermal
 
+# %% [markdown]
+# For simplicity, we define the thermal conductivity of the unitcell to be 1 and the thermal conductivity of the fill to be 100, which is almost negligible in comparison.
 
 # %%
 thermal_conductivities = ["unitcell" => 1, "fill" => 100.0]
-# %% [markdown]
-# First we load the mesh, get the labels, define a CellField to evaluate the constants on
-# and create functions which work on the tags
 
-# %% tags=["remove-stderr", "hide-output"]
 model = GmshDiscreteModel("mesh.msh")
 Ω = Triangulation(model)
 dΩ = Measure(Ω, 1)
@@ -163,10 +173,19 @@ thermal_conductivities =
 ϵ_conductivities(tag) = thermal_conductivities[tag]
 
 # %% [markdown]
-# The next step is to define the boundary conditions, this can be done simply via julia-dicts:
+# We define the temperatures at both sides of the unitcell to define the direction in which we want to estimate the thermal conductivity.
+# In other directions the boundaries are considered to be insulating/mirroring.
 
 # %% tags=["remove-stderr", "hide-output"]
 boundary_temperatures = Dict("left___unitcell" => 100.0, "right___unitcell" => 0.0)
+
+
+# %% [markdown]
+# We start with calculating the temperature distribution.
+# From this, we calculate, analog to how we calculate resistances for electrical simulations first the integral
+# $\int σ \left|\frac{\mathrm{d}T}{\mathrm{d}\vec{x}}\right|^2 dA which gives an equivalent of the power
+# and calculate from this the effective thermal conductivity.
+# We expect the thermal conductivity almost twice as high as the thermal conductivity of the material with lower thermal conductivity.
 
 # %% tags=["remove-stderr"]
 T0 = calculate_temperature(
@@ -185,6 +204,9 @@ power = abs(
         )dΩ,
     ),
 )
-#println(∑(∫(1)dΩ))
-(power / temperature_difference^2) /
-((unitcell_thickness * unitcell_width) / unitcell_length)
+
+println(
+    "Effective thermal conductivity: ",
+    (power / temperature_difference^2) /
+    ((unitcell_thickness * unitcell_width) / unitcell_length),
+)
